dec12932b4a1ad033aecd252d6e5493b54567ccf
Julian Tescher <jatescher@gmail.com>
2019-11-29 19:44:19 -0800

    Make trace and metrics features optional

--- src/sdk/mod.rs
+++ src/sdk/mod.rs
@@ -6,9 +6,14 @@
 //! facilitates the delivery of telemetry data to storage systems
 //! through `Exporter`s. These can be configured on `Tracer` and
 //! `Meter` creation.
+#[cfg(feature = "metrics")]
 pub mod metrics;
+#[cfg(feature = "trace")]
 pub mod trace;
 
+#[cfg(feature = "trace")]
 pub use crate::exporter::trace::jaeger::AllSampler as AlwaysSample;
+#[cfg(feature = "metrics")]
 pub use metrics::{LabelSet, Meter};
+#[cfg(feature = "trace")]
 pub use trace::{config::Config, provider::Provider, span::Span, tracer::Tracer};
