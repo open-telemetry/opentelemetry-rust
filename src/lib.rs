660321e75d69e484cac1cd4cde2bcc7a2dfe8488
Jacob Kiesel <jacob.kiesel@vivint.com>
2020-02-21 11:17:57 -0700

    Provide skeleton implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+use {
+    opentelemetry::exporter::trace::{ExportResult, SpanData, SpanExporter},
+    std::{any::Any, sync::Arc},
+};
+
+/// Exports opentelemetry tracing spans to Google StackDriver.
+#[derive(Debug)]
+pub struct StackDriverExporter {}
+
+impl SpanExporter for StackDriverExporter {
+    fn export(&self, batch: Vec<Arc<SpanData>>) -> ExportResult {
+        ExportResult::Success
+    }
+
+    fn shutdown(&self) {}
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
