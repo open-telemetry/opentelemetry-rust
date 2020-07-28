27ae9afe1f1846b1030fe6fefd61cc73fce58337
Andreas Wiede <andreas.wiede@mapbox.com>
2020-07-28 14:52:29 -0400

    Init otlp crate
    Init otlp crate.

---
+++ opentelemetry-otlp/src/lib.rs
@@ -0,0 +1,25 @@
+use opentelemetry::exporter::trace::{SpanExporter, SpanData, ExportResult};
+use std::sync::Arc;
+
+#[derive(Debug)]
+pub struct Exporter {
+
+}
+
+impl SpanExporter for Exporter {
+    fn export(&self, batch: Vec<Arc<SpanData>>) -> ExportResult {
+        unimplemented!()
+    }
+
+    fn shutdown(&self) {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
