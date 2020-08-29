6768e26c86619e0237b554b7a3c13dfdadca75a4
Andreas Wiede <andreas.wiede@mapbox.com>
2020-08-29 13:04:43 -0400

    Add X-Ray ID Generator (#201)
    Add ID Generator that builds traces in a format that can be backed out
    into an AWS X-Ray compliant trace-id by the OTel collector.

--- opentelemetry-contrib/src/lib.rs
+++ opentelemetry-contrib/src/lib.rs
@@ -5,7 +5,10 @@
 //!
 //! Typically, those include vendor specific propagators.
 
+mod id_generator;
 mod trace_propagator;
+
+pub use id_generator::aws_xray_id_generator::XrayIdGenerator;
 
 pub use trace_propagator::{
     b3_propagator::{B3Encoding, B3Propagator},
