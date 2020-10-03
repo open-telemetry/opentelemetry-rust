afa8bce9c0f21599ff005fd85e4e55c0e79e954a
Julian Tescher <jatescher@gmail.com>
2020-10-03 12:17:23 -0700

    Use last value aggregator for value observers

--- src/sdk/metrics/aggregators/mod.rs
+++ src/sdk/metrics/aggregators/mod.rs
@@ -9,7 +9,7 @@ mod min_max_sum_count;
 mod sum;
 
 pub use array::{array, ArrayAggregator};
-pub use ddsketch::{ddsketch, DDSKetchAggregator};
+pub use ddsketch::{ddsketch, DDSKetchAggregator, DDSketchConfig};
 pub use histogram::{histogram, HistogramAggregator};
 pub use last_value::{last_value, LastValueAggregator};
 pub use min_max_sum_count::{min_max_sum_count, MinMaxSumCountAggregator};
