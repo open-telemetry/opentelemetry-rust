8b759a6656787a7dec80e667d1f01d40d986d30d
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-10-31 12:58:37 +0100

    Move opentelemetry crate into a directory to flatten the workspace
    This makes working with cargo easier, because many commands (like cargo
    check, cargo test) will apply to all crates by default, rather than
    only selecting the crate in the top-level directory.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,9 @@
 set -eu
 
 cargo test --all "$@"
-cargo test --all "$@" --features="default serialize base64_format binary_propagator"
 
 # See https://github.com/rust-lang/cargo/issues/5364
+cargo test --manifest-path=opentelemetry/Cargo.toml --all-features
 cargo test --manifest-path=opentelemetry-contrib/Cargo.toml --all-features
+cargo test --manifest-path=opentelemetry-jaeger/Cargo.toml --all-features
+cargo test --manifest-path=opentelemetry-zipkin/Cargo.toml --all-features
