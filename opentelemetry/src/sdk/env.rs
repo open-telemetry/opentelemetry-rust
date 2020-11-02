7e3301bf531eac36822a2ed6e1d1d1c57e0c35ea
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-11-02 21:41:43 +0100

    Rewrite construct_otel_resources() to prevent unnecessary allocation

--- opentelemetry/src/sdk/env.rs
+++ opentelemetry/src/sdk/env.rs
@@ -3,7 +3,7 @@
 //! Implementation of `ResourceDetector` to extract a `Resource` from environment
 //! variables.
 use crate::sdk::{resource::ResourceDetector, Resource};
-use crate::{Key, KeyValue, Value};
+use crate::KeyValue;
 use std::env;
 use std::time::Duration;
 
@@ -45,20 +45,16 @@ impl Default for EnvResourceDetector {
 /// Extract key value pairs and construct a resource from resources string like
 /// key1=value1,key2=value2,...
 fn construct_otel_resources(s: String) -> Resource {
-    let mut key_values = vec![];
-    for entries in s.split_terminator(',') {
-        let key_value_strs = entries.split('=').map(str::trim).collect::<Vec<&str>>();
-        if key_value_strs.len() != 2 {
-            continue;
+    Resource::new(s.split_terminator(',').filter_map(|entry| {
+        let mut parts = entry.splitn(2, '=');
+        let key = parts.next()?.trim();
+        let value = parts.next()?.trim();
+        if value.find('=').is_some() {
+            return None;
         }
 
-        let key = Key::from(key_value_strs.get(0).unwrap().to_string());
-        let value = Value::String(key_value_strs.get(1).unwrap().to_string());
-
-        key_values.push(KeyValue::new(key, value));
-    }
-
-    Resource::new(key_values)
+        Some(KeyValue::new(key.to_owned(), value.to_owned()))
+    }))
 }
 
 #[cfg(test)]
