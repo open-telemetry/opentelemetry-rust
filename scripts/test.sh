84ed7ea058a00c7a78df39f6968b4de4b2d7dbd7
Dirkjan Ochtman <dirkjan@ochtman.nl>
2021-01-04 12:43:22 +0100

    opentelemetry: make tests pass without default features

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,7 @@ set -eu
 cargo test --all "$@"
 
 # See https://github.com/rust-lang/cargo/issues/5364
+cargo test --manifest-path=opentelemetry/Cargo.toml --no-default-features
 cargo test --manifest-path=opentelemetry/Cargo.toml --all-features
 cargo test --manifest-path=opentelemetry-contrib/Cargo.toml --all-features
 cargo test --manifest-path=opentelemetry-jaeger/Cargo.toml --all-features
