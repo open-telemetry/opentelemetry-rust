0a41043b3fe7c917256642229a940763bec1e809
zhongyang.wu <zhongyang.wu@outlook.com>
2021-04-01 00:53:00 -0400

    fix: otlp docs cannot compile.
    The bug was introduced since we are compiling grpc files using both tonic and grpcio.
    
    The build logs says:
     thread 'main' panicked at 'Error generating protobuf: Os { code: 30, kind: Other, message: "Read-only file system" }
    
    But we already have compiled files in `proto/grpcio`. Thus ignore this error and print a warning message.
    
    ---
    
    Why not use $OUT_DIR? The generated mod file doesn't contain `trace_service_grpc` and `metrics_service_grpc`. Thus, we still need to maintain them in `proto/grpcio`. I'd rather we keep all generated files at the same place

--- opentelemetry-otlp/build.rs
+++ opentelemetry-otlp/build.rs
@@ -28,23 +28,27 @@ fn main() {
         .expect("Error generating protobuf");
 
     #[cfg(feature = "grpc-sys")]
-        compile_grpc_protos(
-        &[
-            "src/proto/opentelemetry-proto/opentelemetry/proto/common/v1/common.proto",
-            "src/proto/opentelemetry-proto/opentelemetry/proto/resource/v1/resource.proto",
-            "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace.proto",
-            "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace_config.proto",
-            "src/proto/opentelemetry-proto/opentelemetry/proto/collector/trace/v1/trace_service.proto",
-            "src/proto/opentelemetry-proto/opentelemetry/proto/metrics/v1/metrics.proto",
-            "src/proto/opentelemetry-proto/opentelemetry/proto/collector/metrics/v1/metrics_service.proto",
-        ],
-        &["src/proto/opentelemetry-proto/"],
-        "src/proto/grpcio",
-        Some(Customize {
-            expose_fields: Some(true),
-            serde_derive: Some(true),
-            ..Default::default()
-        }),
-    )
-        .expect("Error generating protobuf");
+        {
+            let result = compile_grpc_protos(
+                &[
+                    "src/proto/opentelemetry-proto/opentelemetry/proto/common/v1/common.proto",
+                    "src/proto/opentelemetry-proto/opentelemetry/proto/resource/v1/resource.proto",
+                    "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace.proto",
+                    "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace_config.proto",
+                    "src/proto/opentelemetry-proto/opentelemetry/proto/collector/trace/v1/trace_service.proto",
+                    "src/proto/opentelemetry-proto/opentelemetry/proto/metrics/v1/metrics.proto",
+                    "src/proto/opentelemetry-proto/opentelemetry/proto/collector/metrics/v1/metrics_service.proto",
+                ],
+                &["src/proto/opentelemetry-proto/"],
+                "src/proto/grpcio",
+                Some(Customize {
+                    expose_fields: Some(true),
+                    serde_derive: Some(true),
+                    ..Default::default()
+                }),
+            );
+            if let Err(err) = result {
+                println!("cargo:warning=Error generating protobuf: {:?}", err);
+            }
+        }
 }
