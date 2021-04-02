2d92cfc056a190a647382f13ed1f9d13f2b3ce60
zhongyang.wu <zhongyang.wu@outlook.com>
2021-04-01 22:23:15 -0400

    feat: move grpcio's file compilation into tests

--- opentelemetry-otlp/build.rs
+++ opentelemetry-otlp/build.rs
@@ -1,12 +1,8 @@
-#[cfg(feature = "grpc-sys")]
-extern crate protoc_grpcio;
-
-#[cfg(feature = "grpc-sys")]
-use protobuf_codegen::Customize;
-
-#[cfg(feature = "grpc-sys")]
-use protoc_grpcio::compile_grpc_protos;
-
+// Grpc related files used by tonic are generated here. Those files re-generate for each build
+// so it's up to date.
+//
+// Grpc related files used by grpcio are maintained at src/proto/grpcio. tests/grpc_build.rs makes
+// sure they are up to date.
 fn main() {
     #[cfg(feature = "tonic")]
         tonic_build::configure()
@@ -26,29 +22,4 @@ fn main() {
             &["src/proto/opentelemetry-proto"],
         )
         .expect("Error generating protobuf");
-
-    #[cfg(feature = "grpc-sys")]
-    {
-        let result = compile_grpc_protos(
-                &[
-                    "src/proto/opentelemetry-proto/opentelemetry/proto/common/v1/common.proto",
-                    "src/proto/opentelemetry-proto/opentelemetry/proto/resource/v1/resource.proto",
-                    "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace.proto",
-                    "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace_config.proto",
-                    "src/proto/opentelemetry-proto/opentelemetry/proto/collector/trace/v1/trace_service.proto",
-                    "src/proto/opentelemetry-proto/opentelemetry/proto/metrics/v1/metrics.proto",
-                    "src/proto/opentelemetry-proto/opentelemetry/proto/collector/metrics/v1/metrics_service.proto",
-                ],
-                &["src/proto/opentelemetry-proto/"],
-                "src/proto/grpcio",
-                Some(Customize {
-                    expose_fields: Some(true),
-                    serde_derive: Some(true),
-                    ..Default::default()
-                }),
-            );
-        if let Err(err) = result {
-            println!("cargo:warning=Error generating protobuf: {:?}", err);
-        }
-    }
 }
