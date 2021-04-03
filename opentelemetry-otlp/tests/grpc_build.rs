92d722b581f10c6dca0a5b1566f16548107b1148
Zhongyang Wu <zhongyang.wu@outlook.com>
2021-04-03 12:48:59 -0400

    fix: otlp docs cannot compile. (#507)
    The bug was introduced when we started compiling grpc files using both tonic and grpcio.
    
    The build logs says:
    `thread 'main' panicked at 'Error generating protobuf: Os { code: 30, kind: Other, message: "Read-only file system" }`
    
    But we already have compiled files in `proto/grpcio`. Thus we can ignore this error and print a warning message.

---
+++ opentelemetry-otlp/tests/grpc_build.rs
@@ -0,0 +1,55 @@
+use protobuf_codegen::Customize;
+use protoc_grpcio::compile_grpc_protos;
+use std::collections::HashMap;
+use std::path::PathBuf;
+
+// This test helps to keep files generated and used by grpcio update to date.
+// If the test fails, it means the generated files has been changed. Please commit the change
+// and rerun test. It should pass at the second time.
+#[test]
+fn build_grpc() {
+    let before_build = build_content_map();
+    compile_grpc_protos(
+        &[
+            "src/proto/opentelemetry-proto/opentelemetry/proto/common/v1/common.proto",
+            "src/proto/opentelemetry-proto/opentelemetry/proto/resource/v1/resource.proto",
+            "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace.proto",
+            "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace_config.proto",
+            "src/proto/opentelemetry-proto/opentelemetry/proto/collector/trace/v1/trace_service.proto",
+            "src/proto/opentelemetry-proto/opentelemetry/proto/metrics/v1/metrics.proto",
+            "src/proto/opentelemetry-proto/opentelemetry/proto/collector/metrics/v1/metrics_service.proto",
+        ],
+        &["src/proto/opentelemetry-proto/"],
+        "src/proto/grpcio",
+        Some(Customize {
+            expose_fields: Some(true),
+            serde_derive: Some(true),
+            ..Default::default()
+        }),
+    )
+        .expect("Error generating protobuf");
+    let after_build = build_content_map();
+    // we cannot use assert_eq! here because it will print both maps when they don't match, which makes
+    // the error message unreadable.
+    assert!(
+        before_build == after_build,
+        "generated file has changed, please commit the change file and rerun the test"
+    );
+}
+
+fn build_content_map() -> HashMap<PathBuf, String> {
+    let mut map = HashMap::new();
+    let dict =
+        std::fs::read_dir("src/proto/grpcio").expect("cannot open dict of generated grpc files");
+    for entry in dict {
+        if let Ok(entry) = entry {
+            map.insert(
+                entry.path(),
+                std::fs::read_to_string(entry.path()).unwrap_or_else(|_| {
+                    panic!("cannot read from file {}", entry.path().to_string_lossy())
+                }),
+            );
+        }
+    }
+    map
+}
