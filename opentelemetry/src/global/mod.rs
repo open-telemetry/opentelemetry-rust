88e779d53e43d161bb0703808d47728f2fb54a14
Luca Palmieri <lpalmieri@truelayer.com>
2021-05-13 20:41:30 +0100

    Expose the `Error` type to allow users to set custom error handlers (#551)
    Expose the `Error` type in the `global` module to allow users to define their own error handlers. This is currently impossible because `Error` is private and therefore users cannot provide a function that satisfies the constraints of `set_error_handler`.

--- opentelemetry/src/global/mod.rs
+++ opentelemetry/src/global/mod.rs
@@ -144,7 +144,7 @@ mod propagation;
 #[cfg(feature = "trace")]
 mod trace;
 
-pub use error_handler::{handle_error, set_error_handler};
+pub use error_handler::{handle_error, set_error_handler, Error};
 #[cfg(feature = "metrics")]
 #[cfg_attr(docsrs, doc(cfg(feature = "metrics")))]
 pub use metrics::{
