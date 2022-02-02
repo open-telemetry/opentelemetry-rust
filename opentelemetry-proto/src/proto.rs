dd0e756a154807bb05ffe0755659fe70ee9c0a31
Matthijs Brobbel <m1brobbel@gmail.com>
2022-02-02 13:04:35 +0100

    Add logs to opentelemetry-proto crate

--- opentelemetry-proto/src/proto.rs
+++ opentelemetry-proto/src/proto.rs
@@ -1,40 +1,52 @@
 #[cfg(feature = "gen-tonic")]
 pub mod tonic {
     pub mod collector {
+        pub mod logs {
+            pub mod v1 {
+                tonic::include_proto!("opentelemetry.proto.collector.logs.v1");
+            }
+        }
+
         pub mod metrics {
             pub mod v1 {
-                include!(concat!(env!("OUT_DIR"), "/tonic", "/opentelemetry.proto.collector.metrics.v1.rs"));
+                tonic::include_proto!("opentelemetry.proto.collector.metrics.v1");
             }
         }
 
         pub mod trace {
             pub mod v1 {
-                include!(concat!(env!("OUT_DIR"), "/tonic", "/opentelemetry.proto.collector.trace.v1.rs"));
+                tonic::include_proto!("opentelemetry.proto.collector.trace.v1");
             }
         }
     }
 
     pub mod common {
         pub mod v1 {
-            include!(concat!(env!("OUT_DIR"), "/tonic", "/opentelemetry.proto.common.v1.rs"));
+            tonic::include_proto!("opentelemetry.proto.common.v1");
+        }
+    }
+
+    pub mod logs {
+        pub mod v1 {
+            tonic::include_proto!("opentelemetry.proto.logs.v1");
         }
     }
 
     pub mod metrics {
         pub mod v1 {
-            include!(concat!(env!("OUT_DIR"), "/tonic", "/opentelemetry.proto.metrics.v1.rs"));
+            tonic::include_proto!("opentelemetry.proto.metrics.v1");
         }
     }
 
     pub mod resource {
         pub mod v1 {
-            include!(concat!(env!("OUT_DIR"), "/tonic", "/opentelemetry.proto.resource.v1.rs"));
+            tonic::include_proto!("opentelemetry.proto.resource.v1");
         }
     }
 
     pub mod trace {
         pub mod v1 {
-            include!(concat!(env!("OUT_DIR"), "/tonic", "/opentelemetry.proto.trace.v1.rs"));
+            tonic::include_proto!("opentelemetry.proto.trace.v1");
         }
     }
 
