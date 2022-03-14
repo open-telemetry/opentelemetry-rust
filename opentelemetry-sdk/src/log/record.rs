085b5987390a914ba6d8ebcb6f502eb5e9f0f4ac
Vibhav Pant <vibhavp@gmail.com>
2022-03-14 18:44:27 +0530

    Add LogRecordBuilder, documentation for Severity.

--- opentelemetry-sdk/src/log/record.rs
+++ opentelemetry-sdk/src/log/record.rs
@@ -1,4 +1,4 @@
-use opentelemetry_api::trace::{SpanId, TraceFlags, TraceId};
+use opentelemetry_api::trace::{SpanContext, SpanId, TraceContextExt, TraceFlags, TraceId};
 use serde_json::{Number, Value};
 use std::{borrow::Cow, collections::BTreeMap, time::SystemTime};
 
@@ -129,28 +129,181 @@ impl From<Number> for Any {
 /// A normalized severity value.
 #[derive(Debug, Copy, Clone)]
 pub enum Severity {
+    /// TRACE
     Trace = 1,
+    /// TRACE2
     Trace2 = 2,
+    /// TRACE3
     Trace3 = 3,
+    /// TRACE4
     Trace4 = 4,
+    /// DEBUG
     Debug = 5,
+    /// DEBUG2
     Debug2 = 6,
+    /// DEBUG3
     Debug3 = 7,
+    /// DEBUG4
     Debug4 = 8,
+    /// INFO
     Info = 9,
+    /// INFO2
     Info2 = 10,
+    /// INFO3
     Info3 = 11,
+    /// INFO4
     Info4 = 12,
+    /// WARN
     Warn = 13,
+    /// WARN2
     Warn2 = 14,
+    /// WARN3
     Warn3 = 15,
+    /// WARN4
     Warn4 = 16,
+    /// ERROR
     Error = 17,
+    /// ERROR2
     Error2 = 18,
+    /// ERROR3
     Error3 = 19,
+    /// ERROR4
     Error4 = 20,
+    /// FATAL
     Fatal = 21,
+    /// FATAL2
     Fatal2 = 22,
+    /// FATAL3
     Fatal3 = 23,
+    /// FATAL4
     Fatal4 = 24,
+}
+
+/// A builder for [`LogRecord`] values.
+#[derive(Debug, Clone)]
+pub struct LogRecordBuilder {
+    record: LogRecord,
+}
+
+impl LogRecordBuilder {
+    /// Assign timestamp
+    pub fn with_timestamp(self, timestamp: SystemTime) -> Self {
+        Self {
+            record: LogRecord {
+                timestamp: Some(timestamp),
+                ..self.record
+            },
+        }
+    }
+
+    /// Assign the record's [`TraceContext`]
+    pub fn with_span_context(self, span_context: &SpanContext) -> Self {
+        Self {
+            record: LogRecord {
+                trace_context: Some(TraceContext {
+                    span_id: span_context.span_id(),
+                    trace_id: span_context.trace_id(),
+                    trace_flags: Some(span_context.trace_flags()),
+                }),
+                ..self.record
+            },
+        }
+    }
+
+    /// Assign the record's [`TraceContext`] from a `TraceContextExt` trait
+    pub fn with_context<T>(self, context: &T) -> Self
+    where
+        T: TraceContextExt,
+    {
+        if context.has_active_span() {
+            self.with_span_context(context.span().span_context())
+        } else {
+            self
+        }
+    }
+
+    /// Assign severity text
+    pub fn with_severity_text<T>(self, severity: T) -> Self
+    where
+        T: Into<Cow<'static, str>>,
+    {
+        Self {
+            record: LogRecord {
+                severity_text: Some(severity.into()),
+                ..self.record
+            },
+        }
+    }
+
+    /// Assign severity number
+    pub fn with_severity_number<T>(self, severity: Severity) -> Self {
+        Self {
+            record: LogRecord {
+                severity_number: Some(severity),
+                ..self.record
+            },
+        }
+    }
+
+    /// Assign name
+    pub fn with_name<T>(self, name: T) -> Self
+    where
+        T: Into<Cow<'static, str>>,
+    {
+        Self {
+            record: LogRecord {
+                name: Some(name.into()),
+                ..self.record
+            },
+        }
+    }
+
+    /// Assign body
+    pub fn with_body<T>(self, body: Any) -> Self {
+        Self {
+            record: LogRecord {
+                body: Some(body),
+                ..self.record
+            },
+        }
+    }
+
+    /// Assign resource
+    pub fn with_resource(self, resource: BTreeMap<Cow<'static, str>, Any>) -> Self {
+        Self {
+            record: LogRecord {
+                resource: Some(resource),
+                ..self.record
+            },
+        }
+    }
+
+    /// Assign attributes, overriding previously set attributes
+    pub fn with_attributes(self, attributes: BTreeMap<Cow<'static, str>, Any>) -> Self {
+        Self {
+            record: LogRecord {
+                attributes: Some(attributes),
+                ..self.record
+            },
+        }
+    }
+
+    /// Set a single attribute for this record
+    pub fn with_attribute<T>(mut self, key: T, value: Any) -> Self
+    where
+        T: Into<Cow<'static, str>>,
+    {
+        if let Some(ref mut map) = self.record.attributes {
+            map.insert(key.into(), value);
+        } else {
+            self.record.attributes = Some(BTreeMap::from([(key.into(), value)]));
+        }
+
+        self
+    }
+
+    /// Build the record, consuming the Builder
+    pub fn build(self) -> LogRecord {
+        self.record
+    }
 }
