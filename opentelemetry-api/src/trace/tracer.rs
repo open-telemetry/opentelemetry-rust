27226209456068411259a3789622790f10e54268
Luca Palmieri <lpalmieri@truelayer.com>
2022-05-20 09:29:07 +0200

    Minimise breakages for existing users.

--- opentelemetry-api/src/trace/tracer.rs
+++ opentelemetry-api/src/trace/tracer.rs
@@ -4,6 +4,7 @@ use crate::{
     Context, Key, KeyValue, Value,
 };
 use std::borrow::Cow;
+use std::iter::FromIterator;
 use std::time::SystemTime;
 
 /// The interface for constructing [`Span`]s.
@@ -325,8 +326,25 @@ impl SpanBuilder {
         }
     }
 
-    /// Assign span attributes
-    pub fn with_attributes(self, attributes: OrderMap<Key, Value>) -> Self {
+    /// Assign span attributes from an iterable.
+    ///
+    /// Check out [`SpanBuilder::with_attributes_map`] to assign span attributes
+    /// via an [`OrderMap`] instance.
+    pub fn with_attributes<I>(self, attributes: I) -> Self
+    where
+        I: IntoIterator<Item = KeyValue>
+    {
+        SpanBuilder {
+            attributes: Some(OrderMap::from_iter(attributes.into_iter())),
+            ..self
+        }
+    }
+
+    /// Assign span attributes.
+    ///
+    /// Check out [`SpanBuilder::with_attributes`] to assign span attributes
+    /// from an iterable of [`KeyValue`]s.
+    pub fn with_attributes_map(self, attributes: OrderMap<Key, Value>) -> Self {
         SpanBuilder {
             attributes: Some(attributes),
             ..self
