89f0dd787f5bc301833924123ebdedf21611c0a1
Zhongyang Wu <zhongyang.wu@outlook.com>
2022-07-09 19:09:47 -0700

    chore(jaeger): add integration tests to github action.

--- scripts/integration-test.sh
+++
@@ -1,3 +0,0 @@
-COMPOSE_FILE=./opentelemetry-jaeger/tests/docker-compose.yaml
-docker-compose -f $COMPOSE_FILE down -v &&
-docker-compose -f $COMPOSE_FILE up --build --exit-code-from opentelemetry-jaeger
