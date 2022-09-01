9ea1ee77af8577264ddc21e9f1e009aa38437bf8
Zhongyang Wu <zhongyang.wu@outlook.com>
2022-09-01 00:36:37 -0700

    chore(common): bump MSRV to 1.56 (#866)
    * chore(common): bump MSRV to 1.56
    
    * chore(common): bump edition to 2021
    
    * fix(jaeger): clippy issue
    
    * fix: patch_depencencies.sh -> patch_dependencies.sh

--- opentelemetry/src/lib.rs
+++ opentelemetry/src/lib.rs
@@ -2,7 +2,7 @@
 //! services to capture distributed traces and metrics from your application. You
 //! can analyze them using [Prometheus], [Jaeger], and other observability tools.
 //!
-//! *Compiler support: [requires `rustc` 1.49+][msrv]*
+//! *Compiler support: [requires `rustc` 1.56+][msrv]*
 //!
 //! [Prometheus]: https://prometheus.io
 //! [Jaeger]: https://www.jaegertracing.io
@@ -192,7 +192,7 @@
 //! ## Supported Rust Versions
 //!
 //! OpenTelemetry is built against the latest stable release. The minimum
-//! supported version is 1.49. The current OpenTelemetry version is not
+//! supported version is 1.56. The current OpenTelemetry version is not
 //! guaranteed to build on Rust versions earlier than the minimum supported
 //! version.
 //!
