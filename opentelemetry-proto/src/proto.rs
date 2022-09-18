7dd21dc142c2286571eb676d3b1cb7c28693a100
Zhongyang Wu <zhongyang.wu@outlook.com>
2022-09-18 16:21:18 -0700

    feat(proto): remove build script and generate files using unit tests.

--- opentelemetry-proto/src/proto.rs
+++ opentelemetry-proto/src/proto.rs
@@ -1,26 +1,31 @@
 #[cfg(feature = "gen-tonic")]
 /// Generated files using [`tonic`](https://docs.rs/crate/grpcio) and [`prost`](https://docs.rs/crate/protobuf/latest)
 pub mod tonic {
+    macro_rules! include_tonic {
+    ($package: tt) => {
+            include!(concat!("proto/tonic", concat!("/", $package, ".rs")));
+        };
+    }
     /// Service stub and clients
     pub mod collector {
         #[cfg(feature = "logs")]
         pub mod logs {
             pub mod v1 {
-                tonic::include_proto!("opentelemetry.proto.collector.logs.v1");
+                include_tonic!("opentelemetry.proto.collector.logs.v1");
             }
         }
 
         #[cfg(feature = "metrics")]
         pub mod metrics {
             pub mod v1 {
-                tonic::include_proto!("opentelemetry.proto.collector.metrics.v1");
+                include_tonic!("opentelemetry.proto.collector.metrics.v1");
             }
         }
 
         #[cfg(feature = "traces")]
         pub mod trace {
             pub mod v1 {
-                tonic::include_proto!("opentelemetry.proto.collector.trace.v1");
+                include_tonic!("opentelemetry.proto.collector.trace.v1");
             }
         }
     }
@@ -28,7 +33,7 @@ pub mod tonic {
     /// Common types used across all signals
     pub mod common {
         pub mod v1 {
-            tonic::include_proto!("opentelemetry.proto.common.v1");
+            include_tonic!("opentelemetry.proto.common.v1");
         }
     }
 
@@ -36,7 +41,7 @@ pub mod tonic {
     /// Generated types used in logging.
     pub mod logs {
         pub mod v1 {
-            tonic::include_proto!("opentelemetry.proto.logs.v1");
+            include_tonic!("opentelemetry.proto.logs.v1");
         }
     }
 
@@ -44,14 +49,14 @@ pub mod tonic {
     /// Generated types used in metrics.
     pub mod metrics {
         pub mod v1 {
-            tonic::include_proto!("opentelemetry.proto.metrics.v1");
+            include_tonic!("opentelemetry.proto.metrics.v1");
         }
     }
 
     /// Generated types used in resources.
     pub mod resource {
         pub mod v1 {
-            tonic::include_proto!("opentelemetry.proto.resource.v1");
+            include_tonic!("opentelemetry.proto.resource.v1");
         }
     }
 
@@ -59,7 +64,7 @@ pub mod tonic {
     /// Generated types used in traces.
     pub mod trace {
         pub mod v1 {
-            tonic::include_proto!("opentelemetry.proto.trace.v1");
+            include_tonic!("opentelemetry.proto.trace.v1");
         }
     }
 
--- opentelemetry-proto/tests/grpc_build.rs
+++ opentelemetry-proto/tests/grpc_build.rs
@@ -3,33 +3,52 @@ use protoc_grpcio::compile_grpc_protos;
 use std::collections::HashMap;
 use std::path::PathBuf;
 
+const GRPCIO_PROTO_DIR: &str = "src/proto/grpcio";
+const GRPCIO_PROTO_FILES: &[&str] = &[
+    "src/proto/opentelemetry-proto/opentelemetry/proto/common/v1/common.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/resource/v1/resource.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace_config.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/collector/trace/v1/trace_service.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/metrics/v1/metrics.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/collector/metrics/v1/metrics_service.proto",
+    "src/proto/tracez.proto",
+];
+const GRPCIO_INCLUDES: &[&str] = &["src/proto/opentelemetry-proto/", "src/proto"];
+const GRPCIO_OUT_DIR: &str = "src/proto/grpcio";
+
+const TONIC_PROTO_DIR: &str = "src/proto/tonic";
+const TONIC_PROTO_FILES: &[&str] = &[
+    "src/proto/opentelemetry-proto/opentelemetry/proto/common/v1/common.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/resource/v1/resource.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace_config.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/collector/trace/v1/trace_service.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/metrics/v1/metrics.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/collector/metrics/v1/metrics_service.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/logs/v1/logs.proto",
+    "src/proto/opentelemetry-proto/opentelemetry/proto/collector/logs/v1/logs_service.proto",
+];
+const TONIC_INCLUDES: &[&str] = &["src/proto/opentelemetry-proto"];
+
 // This test helps to keep files generated and used by grpcio update to date.
 // If the test fails, it means the generated files has been changed. Please commit the change
 // and rerun test. It should pass at the second time.
 #[test]
 fn build_grpc() {
-    let before_build = build_content_map();
+    let before_build = build_content_map(GRPCIO_PROTO_DIR);
     compile_grpc_protos(
-        &[
-            "src/proto/opentelemetry-proto/opentelemetry/proto/common/v1/common.proto",
-            "src/proto/opentelemetry-proto/opentelemetry/proto/resource/v1/resource.proto",
-            "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace.proto",
-            "src/proto/opentelemetry-proto/opentelemetry/proto/trace/v1/trace_config.proto",
-            "src/proto/opentelemetry-proto/opentelemetry/proto/collector/trace/v1/trace_service.proto",
-            "src/proto/opentelemetry-proto/opentelemetry/proto/metrics/v1/metrics.proto",
-            "src/proto/opentelemetry-proto/opentelemetry/proto/collector/metrics/v1/metrics_service.proto",
-            "src/proto/tracez.proto"
-        ],
-        &["src/proto/opentelemetry-proto/", "src/proto"],
-        "src/proto/grpcio",
+        GRPCIO_PROTO_FILES,
+        GRPCIO_INCLUDES,
+        GRPCIO_OUT_DIR,
         Some(Customize {
             expose_fields: Some(true),
             serde_derive: Some(true),
             ..Default::default()
         }),
     )
-        .expect("Error generating protobuf");
-    let after_build = build_content_map();
+    .expect("Error generating protobuf");
+    let after_build = build_content_map(GRPCIO_PROTO_DIR);
     // we cannot use assert_eq! here because it will print both maps when they don't match, which
     // makes the error message unreadable.
     // If you find the test passed locally but not in CI pipeline. Try update the dependency. It may
@@ -41,9 +60,27 @@ fn build_grpc() {
     );
 }
 
-fn build_content_map() -> HashMap<PathBuf, String> {
-    std::fs::read_dir("src/proto/grpcio")
-        .expect("cannot open dict of generated grpc files")
+#[test]
+fn build_tonic() {
+    let before_build = build_content_map(TONIC_PROTO_DIR);
+
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .out_dir(TONIC_PROTO_DIR)
+        .compile(TONIC_PROTO_FILES, TONIC_INCLUDES)
+        .expect("cannot compile protobuf using tonic");
+    let after_build = build_content_map(TONIC_PROTO_DIR);
+    // see build_grpc for why we cannot use assert! here.
+    assert!(
+        before_build == after_build,
+        "generated file has changed, please commit the change file and rerun the test"
+    );
+}
+
+fn build_content_map(path: &'static str) -> HashMap<PathBuf, String> {
+    std::fs::read_dir(path)
+        .expect("cannot open dictionary of generated grpc files")
         .into_iter()
         .flatten()
         .map(|entry| {
