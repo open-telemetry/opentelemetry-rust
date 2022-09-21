86d26877065c4ff8baaa4a2e925dbfbd7550a6cb
Zhongyang Wu <zhongyang.wu@outlook.com>
2022-09-20 22:05:09 -0700

    fix(proto): replace macro! with #[path]

--- opentelemetry-proto/src/proto.rs
+++ opentelemetry-proto/src/proto.rs
@@ -1,71 +1,68 @@
 #[cfg(feature = "gen-tonic")]
+#[path = "proto/tonic"]
 /// Generated files using [`tonic`](https://docs.rs/crate/grpcio) and [`prost`](https://docs.rs/crate/protobuf/latest)
 pub mod tonic {
-    macro_rules! include_tonic {
-    ($package: tt) => {
-            include!(concat!("proto/tonic", concat!("/", $package, ".rs")));
-        };
-    }
     /// Service stub and clients
+    #[path = ""]
     pub mod collector {
         #[cfg(feature = "logs")]
+        #[path = ""]
         pub mod logs {
-            pub mod v1 {
-                include_tonic!("opentelemetry.proto.collector.logs.v1");
-            }
+            #[path = "opentelemetry.proto.collector.logs.v1.rs"]
+            pub mod v1;
         }
 
         #[cfg(feature = "metrics")]
+        #[path = ""]
         pub mod metrics {
-            pub mod v1 {
-                include_tonic!("opentelemetry.proto.collector.metrics.v1");
-            }
+            #[path = "opentelemetry.proto.collector.metrics.v1.rs"]
+            pub mod v1;
         }
 
         #[cfg(feature = "traces")]
+        #[path = ""]
         pub mod trace {
-            pub mod v1 {
-                include_tonic!("opentelemetry.proto.collector.trace.v1");
-            }
+            #[path = "opentelemetry.proto.collector.trace.v1.rs"]
+            pub mod v1;
         }
     }
 
     /// Common types used across all signals
+    #[path = ""]
     pub mod common {
-        pub mod v1 {
-            include_tonic!("opentelemetry.proto.common.v1");
-        }
+        #[path = "opentelemetry.proto.common.v1.rs"]
+        pub mod v1;
     }
 
-    #[cfg(feature = "logs")]
     /// Generated types used in logging.
+    #[cfg(feature = "logs")]
+    #[path = ""]
     pub mod logs {
-        pub mod v1 {
-            include_tonic!("opentelemetry.proto.logs.v1");
-        }
+        #[path = "opentelemetry.proto.logs.v1.rs"]
+        pub mod v1;
     }
 
-    #[cfg(feature = "metrics")]
     /// Generated types used in metrics.
+    #[cfg(feature = "metrics")]
+    #[path = ""]
     pub mod metrics {
-        pub mod v1 {
-            include_tonic!("opentelemetry.proto.metrics.v1");
-        }
+        #[path = "opentelemetry.proto.metrics.v1.rs"]
+        pub mod v1;
     }
 
     /// Generated types used in resources.
+    #[path = ""]
     pub mod resource {
-        pub mod v1 {
-            include_tonic!("opentelemetry.proto.resource.v1");
-        }
+        #[path = "opentelemetry.proto.resource.v1.rs"]
+        pub mod v1;
     }
 
-    #[cfg(feature = "traces")]
     /// Generated types used in traces.
+    #[cfg(feature = "traces")]
+    #[path = ""]
     pub mod trace {
-        pub mod v1 {
-            include_tonic!("opentelemetry.proto.trace.v1");
-        }
+        #[path = "opentelemetry.proto.trace.v1.rs"]
+        pub mod v1;
     }
 
     pub use crate::transform::common::tonic::Attributes;
