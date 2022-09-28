89c6614850ef4a7292fe96b9213a92c3659b79c9
Zhongyang Wu <zhongyang.wu@outlook.com>
2022-09-27 20:03:03 -0700

    fix(commmon): use temporary dir to store the generated file and only modify the `src` dir when necessary.

--- opentelemetry-proto/tests/grpc_build.rs
+++ opentelemetry-proto/tests/grpc_build.rs
@@ -1,9 +1,10 @@
 use protobuf_codegen::Customize;
 use protoc_grpcio::compile_grpc_protos;
 use std::collections::HashMap;
-use std::path::PathBuf;
+use std::path::Path;
+use tempfile::TempDir;
 
-const GRPCIO_PROTO_DIR: &str = "src/proto/grpcio";
+const GRPCIO_OUT_DIR: &str = "src/proto/grpcio";
 const GRPCIO_PROTO_FILES: &[&str] = &[
     "src/proto/opentelemetry-proto/opentelemetry/proto/common/v1/common.proto",
     "src/proto/opentelemetry-proto/opentelemetry/proto/resource/v1/resource.proto",
@@ -15,9 +16,8 @@ const GRPCIO_PROTO_FILES: &[&str] = &[
     "src/proto/tracez.proto",
 ];
 const GRPCIO_INCLUDES: &[&str] = &["src/proto/opentelemetry-proto/", "src/proto"];
-const GRPCIO_OUT_DIR: &str = "src/proto/grpcio";
 
-const TONIC_PROTO_DIR: &str = "src/proto/tonic";
+const TONIC_OUT_DIR: &str = "src/proto/tonic";
 const TONIC_PROTO_FILES: &[&str] = &[
     "src/proto/opentelemetry-proto/opentelemetry/proto/common/v1/common.proto",
     "src/proto/opentelemetry-proto/opentelemetry/proto/resource/v1/resource.proto",
@@ -36,60 +36,79 @@ const TONIC_INCLUDES: &[&str] = &["src/proto/opentelemetry-proto"];
 // and rerun test. It should pass at the second time.
 #[test]
 fn build_grpc() {
-    let before_build = build_content_map(GRPCIO_PROTO_DIR);
+    let before_build = build_content_map(GRPCIO_OUT_DIR);
+
+    let out_dir = TempDir::new().expect("failed to create temp dir to store the generated files");
+
     compile_grpc_protos(
         GRPCIO_PROTO_FILES,
         GRPCIO_INCLUDES,
-        GRPCIO_OUT_DIR,
+        out_dir.path(),
         Some(Customize {
             expose_fields: Some(true),
             serde_derive: Some(true),
             ..Default::default()
         }),
     )
-    .expect("Error generating protobuf");
-    let after_build = build_content_map(GRPCIO_PROTO_DIR);
-    // we cannot use assert_eq! here because it will print both maps when they don't match, which
-    // makes the error message unreadable.
-    // If you find the test passed locally but not in CI pipeline. Try update the dependency. It may
-    // be a new version of protobuf or other dependencies
-    // DO NOT use assert_eq! here as it will print all generated file when proto changes.
-    assert!(
-        before_build == after_build,
-        "generated file has changed, please commit the change file and rerun the test"
-    );
+    .expect("error generating protobuf");
+    let after_build = build_content_map(out_dir.path());
+    ensure_files_are_same(before_build, after_build, GRPCIO_OUT_DIR);
 }
 
 #[test]
 fn build_tonic() {
-    let before_build = build_content_map(TONIC_PROTO_DIR);
+    let before_build = build_content_map(TONIC_OUT_DIR);
 
+    let out_dir = TempDir::new().expect("failed to create temp dir to store the generated files");
+
+    // build the generated files into OUT_DIR for now so we don't have to touch the src unless we have to
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
-        .out_dir(TONIC_PROTO_DIR)
+        .out_dir(out_dir.path())
         .compile(TONIC_PROTO_FILES, TONIC_INCLUDES)
         .expect("cannot compile protobuf using tonic");
-    let after_build = build_content_map(TONIC_PROTO_DIR);
-    // see build_grpc for why we cannot use assert! here.
-    assert!(
-        before_build == after_build,
-        "generated file has changed, please commit the change file and rerun the test"
-    );
+
+    let after_build = build_content_map(out_dir.path());
+    ensure_files_are_same(before_build, after_build, TONIC_OUT_DIR);
 }
 
-fn build_content_map(path: &'static str) -> HashMap<PathBuf, String> {
+fn build_content_map(path: impl AsRef<Path>) -> HashMap<String, String> {
     std::fs::read_dir(path)
-        .expect("cannot open dictionary of generated grpc files")
+        .expect("cannot open dictionary of generated files")
         .into_iter()
         .flatten()
         .map(|entry| {
+            let path = entry.path();
+            let file_name = path
+                .file_name()
+                .expect("file name should always exist for generated files");
             (
-                entry.path(),
-                std::fs::read_to_string(entry.path()).unwrap_or_else(|_| {
-                    panic!("cannot read from file {}", entry.path().to_string_lossy())
-                }),
+                file_name.to_string_lossy().to_string(),
+                std::fs::read_to_string(path).expect("cannot read from existing generated file"),
             )
         })
         .collect()
+}
+
+fn ensure_files_are_same(
+    before_build: HashMap<String, String>,
+    after_build: HashMap<String, String>,
+    target_dir: &'static str,
+) {
+    if after_build == before_build {
+        return;
+    }
+
+    if std::env::var("CI").is_ok() {
+        panic!("generated file has changed but it's a CI environment, please rerun this test locally and commit the changes");
+    }
+
+    // if there is at least one changes we will just copy the whole directory over
+    for (file_name, content) in after_build {
+        std::fs::write(Path::new(target_dir).join(file_name), content)
+            .expect("cannot write to the proto generate file. If it's happening in CI env, please return the test locally and commit the change");
+    }
+
+    panic!("generated file has changed, please commit the change file and rerun the test");
 }
