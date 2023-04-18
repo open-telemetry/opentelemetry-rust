482d951fa16da3ed45e90dcec1fb491c29d2f476
Simen Bekkhus <sbekkhus91@gmail.com>
2023-04-18 02:30:45 +0200

    feat: upgrade to tonic 0.9 (#1016)

--- opentelemetry-stackdriver/src/proto/devtools/cloudtrace/v2.rs
+++ opentelemetry-stackdriver/src/proto/devtools/cloudtrace/v2.rs
@@ -506,7 +506,7 @@ pub mod trace_service_client {
         /// Attempt to create a new client by connecting to a given endpoint.
         pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
         where
-            D: std::convert::TryInto<tonic::transport::Endpoint>,
+            D: TryInto<tonic::transport::Endpoint>,
             D::Error: Into<StdError>,
         {
             let conn = tonic::transport::Endpoint::new(dst)?.connect().await?;
@@ -561,12 +561,28 @@ pub mod trace_service_client {
             self.inner = self.inner.accept_compressed(encoding);
             self
         }
+        /// Limits the maximum size of a decoded message.
+        ///
+        /// Default: `4MB`
+        #[must_use]
+        pub fn max_decoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_decoding_message_size(limit);
+            self
+        }
+        /// Limits the maximum size of an encoded message.
+        ///
+        /// Default: `usize::MAX`
+        #[must_use]
+        pub fn max_encoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_encoding_message_size(limit);
+            self
+        }
         /// Batch writes new spans to new or existing traces. You cannot update
         /// existing spans.
         pub async fn batch_write_spans(
             &mut self,
             request: impl tonic::IntoRequest<super::BatchWriteSpansRequest>,
-        ) -> Result<tonic::Response<()>, tonic::Status> {
+        ) -> std::result::Result<tonic::Response<()>, tonic::Status> {
             self.inner.ready().await.map_err(|e| {
                 tonic::Status::new(
                     tonic::Code::Unknown,
@@ -577,13 +593,18 @@ pub mod trace_service_client {
             let path = http::uri::PathAndQuery::from_static(
                 "/google.devtools.cloudtrace.v2.TraceService/BatchWriteSpans",
             );
-            self.inner.unary(request.into_request(), path, codec).await
+            let mut req = request.into_request();
+            req.extensions_mut().insert(GrpcMethod::new(
+                "google.devtools.cloudtrace.v2.TraceService",
+                "BatchWriteSpans",
+            ));
+            self.inner.unary(req, path, codec).await
         }
         /// Creates a new span.
         pub async fn create_span(
             &mut self,
             request: impl tonic::IntoRequest<super::Span>,
-        ) -> Result<tonic::Response<super::Span>, tonic::Status> {
+        ) -> std::result::Result<tonic::Response<super::Span>, tonic::Status> {
             self.inner.ready().await.map_err(|e| {
                 tonic::Status::new(
                     tonic::Code::Unknown,
@@ -594,7 +615,12 @@ pub mod trace_service_client {
             let path = http::uri::PathAndQuery::from_static(
                 "/google.devtools.cloudtrace.v2.TraceService/CreateSpan",
             );
-            self.inner.unary(request.into_request(), path, codec).await
+            let mut req = request.into_request();
+            req.extensions_mut().insert(GrpcMethod::new(
+                "google.devtools.cloudtrace.v2.TraceService",
+                "CreateSpan",
+            ));
+            self.inner.unary(req, path, codec).await
         }
     }
 }
