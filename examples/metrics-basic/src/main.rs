426906912487fa2055f982239bf903e4257ea703
Cijo Thomas <cijo.thomas@gmail.com>
2023-05-11 14:57:56 -0700

    fmt fix final

--- examples/metrics-basic/src/main.rs
+++ examples/metrics-basic/src/main.rs
@@ -2,7 +2,6 @@ use opentelemetry_api::metrics::Unit;
 use opentelemetry_api::{metrics::MeterProvider as _, Context, KeyValue};
 use opentelemetry_sdk::metrics::{MeterProvider, PeriodicReader};
 use opentelemetry_sdk::{runtime, Resource};
-use opentelemetry_stdout;
 use std::error::Error;
 
 fn init_meter_provider() -> MeterProvider {
