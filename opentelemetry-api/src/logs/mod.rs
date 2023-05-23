ecd53973dc8a881243a33105be4725b9eeabd5c6
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2023-05-22 22:11:50 -0700

    fix ordermap usage

--- opentelemetry-api/src/logs/mod.rs
+++ opentelemetry-api/src/logs/mod.rs
@@ -13,8 +13,14 @@ pub use logger::{Logger, LoggerProvider};
 pub use noop::NoopLoggerProvider;
 pub use record::{AnyValue, LogRecord, LogRecordBuilder, Severity, TraceContext};
 
+use std::collections::hash_map::RandomState;
+
+
 /// Describe the result of operations in log SDK.
 pub type LogResult<T> = Result<T, LogError>;
+
+/// re-export OrderMap to mitigate breaking change
+pub type OrderMap<K, V, S = RandomState> = crate::order_map::OrderMap<K, V, S>;
 
 #[derive(Error, Debug)]
 #[non_exhaustive]
