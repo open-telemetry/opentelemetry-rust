286ce1ff876bbd7bc3494c874abb03e23a4a39b3
Cijo Thomas <cijo.thomas@gmail.com>
2023-05-30 09:02:43 -0700

    Add log appender for tracing

---
+++ opentelemetry-appender-tracing/examples/basic.rs
@@ -0,0 +1,25 @@
+//! run with `$ cargo run --example basic --all-features
+
+use opentelemetry_appender_tracing::{layer};
+use tracing::{info, error};
+use opentelemetry_sdk::{logs::{LoggerProvider, Config}, Resource};
+use opentelemetry_api::KeyValue;
+use tracing_subscriber::prelude::*;
+
+fn main() {
+    let exporter  = opentelemetry_stdout::LogExporter::default();
+    let provider: LoggerProvider = LoggerProvider::builder()
+    .with_config(
+        Config::default().with_resource(Resource::new(vec![KeyValue::new(
+            "service.name",
+            "log-appender-tracing-example",
+        )])),
+    )
+    .with_simple_exporter(exporter)
+    .build();
+    let layer  = layer::OpenTelemetryTracingBridge::new(&provider);
+    tracing_subscriber::registry().with(layer).init();
+
+    info!(fruit = "apple", price = 2.99, message = "hello from from price");
+    drop(provider);
+}
---
+++ opentelemetry-appender-tracing/src/layer.rs
@@ -0,0 +1,124 @@
+use opentelemetry_api::{
+    logs::LoggerProvider, logs::{Severity, AnyValue}, logs::{LogRecord, Logger},
+    OrderMap
+};
+
+use tracing_subscriber::Layer;
+
+const INSTRUMENTATION_LIBRARY_NAME: &str = "opentelemetry-appender-tracing";
+
+/// Visitor to record the message from the event record
+struct EventVisitor<'a> {
+    log_record: &'a mut LogRecord
+}
+
+impl<'a> tracing::field::Visit for EventVisitor<'a> {
+
+    fn record_error(
+        &mut self,
+        field: &tracing::field::Field,
+        value: &(dyn std::error::Error + 'static),
+    ) {
+        if field.name() == "message" {
+            self.log_record.body = Some(value.to_string().into());
+        }
+    }
+
+    fn record_debug(&mut self, field: &tracing::field::Field, value: &dyn std::fmt::Debug) {
+        if field.name() == "message" {
+            self.log_record.body = Some(format!("{value:?}").into());
+        }
+    }
+
+    fn record_f64(&mut self, field: &tracing::field::Field, value: f64) {
+        if let Some(ref mut map) = self.log_record.attributes {
+            map.insert(field.name().into(), value.into());
+        } else {
+            let mut map = OrderMap::with_capacity(1);
+            map.insert(field.name().into(), value.into());
+            self.log_record.attributes = Some(map);
+        }
+    }
+
+    fn record_i64(&mut self, field: &tracing::field::Field, value: i64) {
+        if let Some(ref mut map) = self.log_record.attributes {
+            map.insert(field.name().into(), value.into());
+        } else {
+            let mut map = OrderMap::with_capacity(1);
+            map.insert(field.name().into(), value.into());
+            self.log_record.attributes = Some(map);
+        }
+    }
+
+    fn record_u64(&mut self, _field: &tracing::field::Field, _value: u64) {
+        //Not supported type
+    }
+
+    fn record_bool(&mut self, field: &tracing::field::Field, value: bool) {
+        if let Some(ref mut map) = self.log_record.attributes {
+            map.insert(field.name().into(), value.into());
+        } else {
+            let mut map = OrderMap::with_capacity(1);
+            map.insert(field.name().into(), value.into());
+            self.log_record.attributes = Some(map);
+        }
+    }
+}
+
+impl<'a> EventVisitor<'a>{
+    fn update_severity(&mut self, level: &str){
+        self.log_record.severity_text = Some(level.to_string().into());
+
+        //self.log_record_builder.with_severity_text(level);
+        match level {
+            "Info" => self.log_record.severity_number =  Some(Severity::Info),
+            "Debug" => self.log_record.severity_number =  Some(Severity::Debug),
+            "Trace" => self.log_record.severity_number =  Some(Severity::Trace),
+            "Warn" => self.log_record.severity_number =  Some(Severity::Warn),
+            "Error" =>self.log_record.severity_number =  Some(Severity::Error),
+            _ => self.log_record.severity_number = Some(Severity::Info) // won't reach here
+        };
+    }
+}
+
+pub struct OpenTelemetryTracingBridge <P, L>
+where
+    P : LoggerProvider<Logger = L> + Send +Sync,
+    L : Logger + Send + Sync,
+{
+    logger: L,
+    _phantom: std::marker::PhantomData<P>, // P is not used.
+}
+
+impl<P, L> OpenTelemetryTracingBridge<P, L>
+where
+    P : LoggerProvider<Logger = L> + Send +Sync,
+    L : Logger + Send + Sync,
+{
+    pub fn new(provider: &P) -> Self{
+        OpenTelemetryTracingBridge {
+            logger: provider.logger(INSTRUMENTATION_LIBRARY_NAME),
+            _phantom: Default::default(),
+        }
+    }
+}
+
+impl<S, P, L> Layer<S> for OpenTelemetryTracingBridge<P, L>
+where
+    S: tracing::Subscriber,
+    P : LoggerProvider<Logger = L> + Send +Sync + 'static,
+    L : Logger + Send + Sync + 'static,
+    {
+        fn on_event(
+            &self,
+            event: &tracing::Event<'_>,
+            _ctx: tracing_subscriber::layer::Context<'_, S>
+        ) {
+            let meta = event.metadata();
+            let mut log_record: LogRecord = LogRecord::default();
+            let mut visitor = EventVisitor{log_record: &mut log_record};
+            visitor.update_severity( meta.level().as_str());
+            event.record(&mut visitor);
+            self.logger.emit(log_record);
+        }
+    }
