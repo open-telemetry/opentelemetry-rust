4a56ae294281740a015b6be0200df4653f50d36a
Cijo Thomas <cijo.thomas@gmail.com>
2023-06-18 06:50:07 -0700

    fmt

--- stress/src/metrics.rs
+++ stress/src/metrics.rs
@@ -1,5 +1,8 @@
 use lazy_static::lazy_static;
-use opentelemetry_api::{metrics::{Counter, MeterProvider as _}, KeyValue};
+use opentelemetry_api::{
+    metrics::{Counter, MeterProvider as _},
+    KeyValue,
+};
 use opentelemetry_sdk::metrics::{ManualReader, MeterProvider};
 use rand::Rng;
 use std::borrow::Cow;
@@ -10,8 +13,10 @@ lazy_static! {
     static ref PROVIDER: MeterProvider = MeterProvider::builder()
         .with_reader(ManualReader::builder().build())
         .build();
-    static ref ATTRIBUTE_VALUES: [&'static str; 10] = ["value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8", "value9", "value10"];
-
+    static ref ATTRIBUTE_VALUES: [&'static str; 10] = [
+        "value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8", "value9",
+        "value10"
+    ];
     static ref COUNTER: Counter<u64> = PROVIDER
         .meter(<&str as Into<Cow<'static, str>>>::into("test"))
         .u64_counter("hello")
@@ -28,11 +33,14 @@ fn test_counter() {
     let index_first_attribute = rng.gen_range(0..len);
     let index_second_attribute = rng.gen_range(0..len);
     let index_third_attribute = rng.gen_range(0..len);
-    
+
     // each attribute has 10 possible values, so there are 1000 possible combinations (time-series)
-    COUNTER.add(1, &[
-        KeyValue::new("attribute1", ATTRIBUTE_VALUES[index_first_attribute]),
-        KeyValue::new("attribute2", ATTRIBUTE_VALUES[index_second_attribute]),
-        KeyValue::new("attribute3", ATTRIBUTE_VALUES[index_third_attribute]),
-    ]);
+    COUNTER.add(
+        1,
+        &[
+            KeyValue::new("attribute1", ATTRIBUTE_VALUES[index_first_attribute]),
+            KeyValue::new("attribute2", ATTRIBUTE_VALUES[index_second_attribute]),
+            KeyValue::new("attribute3", ATTRIBUTE_VALUES[index_third_attribute]),
+        ],
+    );
 }
