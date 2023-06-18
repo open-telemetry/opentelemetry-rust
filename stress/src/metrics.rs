08737c004d82c6552845441b42fd523a6e067009
Cijo Thomas <cijo.thomas@gmail.com>
2023-06-17 19:44:54 -0700

    Fix metric stress tests

--- stress/src/metrics.rs
+++ stress/src/metrics.rs
@@ -1,6 +1,7 @@
 use lazy_static::lazy_static;
-use opentelemetry_api::metrics::{Counter, MeterProvider as _};
+use opentelemetry_api::{metrics::{Counter, MeterProvider as _, Histogram}, KeyValue, Value};
 use opentelemetry_sdk::metrics::{ManualReader, MeterProvider};
+use rand::Rng;
 use std::borrow::Cow;
 
 mod throughput;
@@ -9,9 +10,16 @@ lazy_static! {
     static ref PROVIDER: MeterProvider = MeterProvider::builder()
         .with_reader(ManualReader::builder().build())
         .build();
+    static ref AttributeValues: [&'static str; 10] = ["value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8", "value9", "value10"];
+
     static ref COUNTER: Counter<u64> = PROVIDER
         .meter(<&str as Into<Cow<'static, str>>>::into("test"))
         .u64_counter("hello")
+        .init();
+
+    static ref HISTOGRAM: Histogram<f64> = PROVIDER
+        .meter(<&str as Into<Cow<'static, str>>>::into("test"))
+        .f64_histogram("my_histogram")
         .init();
 }
 
@@ -20,5 +28,18 @@ fn main() {
 }
 
 fn test_counter() {
-    COUNTER.add(1, &[]);
+    let mut rng = rand::thread_rng();
+    let index_first_attribute = rng.gen_range(0..AttributeValues.len());
+    let index_second_attribute = rng.gen_range(0..AttributeValues.len());
+    let index_third_attribute = rng.gen_range(0..AttributeValues.len());
+    let index_fourth_attribute = rng.gen_range(0..AttributeValues.len());
+    let index_fifth_attribute = rng.gen_range(0..AttributeValues.len());
+    
+    HISTOGRAM.record(1.0, &[
+        KeyValue::new("attribute1", AttributeValues[index_first_attribute]),
+        KeyValue::new("attribute2", AttributeValues[index_second_attribute]),
+        // KeyValue::new("attribute3", AttributeValues[index_third_attribute]),
+        // KeyValue::new("attribute4", AttributeValues[index_fourth_attribute]),
+        // KeyValue::new("attribute5", AttributeValues[index_fifth_attribute]),
+    ]);
 }
