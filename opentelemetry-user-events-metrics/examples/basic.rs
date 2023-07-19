55dc2a8aebbfac759b487a4b4c46e083575a4b0e
Leighton Chen <lechen@microsoft.com>
2023-07-19 09:45:35 -0700

    cli

--- opentelemetry-user-events-metrics/examples/basic.rs
+++ opentelemetry-user-events-metrics/examples/basic.rs
@@ -8,6 +8,16 @@ use opentelemetry_sdk::{
     runtime, Resource,
 };
 use opentelemetry_user_events_metrics::MetricsExporter;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct CliOptions {
+    #[structopt(short = "a", long = "account", value_name = "ACCOUNT", help = "Sets metrics account")]
+    metrics_account: Option<String>,
+
+    #[structopt(short = "n", long = "namespace", value_name = "NAMESPACE", help = "Sets the metrics namespace")]
+    metrics_namespace: Option<String>,
+}
 
 fn init_metrics(exporter: MetricsExporter) -> MeterProvider {
     let reader = PeriodicReader::builder(exporter, runtime::Tokio).build();
@@ -38,14 +48,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_unit(Unit::new("test_unit"))
         .init();
 
-    c.add(
-        1.0,
-        [
-            KeyValue::new("mykey1", "myvalue1"),
-            KeyValue::new("mykey2", "myvalue2"),
-        ]
-        .as_ref(),
-    );
+    let mut attributes: Vec<KeyValue> = Vec::new();
+    attributes.push(KeyValue::new("mykey1", "myvalue1"));
+    attributes.push(KeyValue::new("mykey2", "myvalue2"));
+    let options = CliOptions::from_args();
+
+    // Access the values of the arguments
+    if let Some(metrics_account) = options.metrics_account {
+        println!("Account: {}", metrics_account);
+        attributes.push(KeyValue::new("_microsoft_metrics_account", metrics_account));
+    }
+
+    if let Some(metrics_namespace) = options.metrics_namespace {
+        println!("Namespace: {}", metrics_namespace);
+        attributes.push(KeyValue::new("_microsoft_metrics_namespace", metrics_namespace));
+    }
+
+    c.add(1.0, &attributes);
 
     meter_provider.shutdown()?;
 
