f0da10f41ea1932cf5cc6debc799bc2f79a621ef
Cijo Thomas <cijo.thomas@gmail.com>
2023-09-08 09:52:41 -0700

    Remove dev deps on log appender (#1249)
    * Remove dev deps on log appender
    
    * comment

--- opentelemetry-sdk/src/testing/logs/in_memory_exporter.rs
+++ opentelemetry-sdk/src/testing/logs/in_memory_exporter.rs
@@ -11,31 +11,19 @@ use std::sync::{Arc, Mutex};
 ///
 /// # Example
 /// ```no_run
-///# use log::{error, info, Level, warn};
-///# use opentelemetry_appender_log::OpenTelemetryLogBridge;
 ///# use opentelemetry_sdk::logs::{BatchLogProcessor, LoggerProvider};
 ///# use opentelemetry_sdk::runtime;
 ///# use opentelemetry_sdk::testing::logs::InMemoryLogsExporter;
 ///
 ///# #[tokio::main]
 ///# async fn main() {
-///    //Create an InMemoryLogsExporter
+///    // Create an InMemoryLogsExporter
 ///    let exporter: InMemoryLogsExporter = InMemoryLogsExporter::default();
 ///    //Create a LoggerProvider and register the exporter
 ///    let logger_provider = LoggerProvider::builder()
 ///        .with_log_processor(BatchLogProcessor::builder(exporter.clone(), runtime::Tokio).build())
 ///        .build();
-
-///    // Setup Log Appender for the log crate.
-///    let otel_log_appender = OpenTelemetryLogBridge::new(&logger_provider);
-///    log::set_boxed_logger(Box::new(otel_log_appender)).unwrap();
-///    log::set_max_level(Level::Info.to_level_filter());
-
-///    // Emit logs using macros from the log crate.
-///    error!("hello from {}. My price is {}", "apple", 2.99);
-///    warn!("warn!");
-///    info!("test log!");
-
+///    // Setup Log Appenders and emit logs. (Not shown here)
 ///    logger_provider.force_flush();
 
 ///    let emitted_logs = exporter.get_emitted_logs().unwrap();
@@ -61,8 +49,6 @@ impl Default for InMemoryLogsExporter {
 ///
 /// ```no_run
 ///# use opentelemetry_sdk::testing::logs::{InMemoryLogsExporter, InMemoryLogsExporterBuilder};
-///# use log::{error, info, Level, warn};
-///# use opentelemetry_appender_log::OpenTelemetryLogBridge;
 ///# use opentelemetry_sdk::logs::{BatchLogProcessor, LoggerProvider};
 ///# use opentelemetry_sdk::runtime;
 ///
@@ -74,14 +60,7 @@ impl Default for InMemoryLogsExporter {
 ///    let logger_provider = LoggerProvider::builder()
 ///        .with_log_processor(BatchLogProcessor::builder(exporter.clone(), runtime::Tokio).build())
 ///        .build();
-///    // Setup Log Appender for the log crate.
-///    let otel_log_appender = OpenTelemetryLogBridge::new(&logger_provider);
-///    log::set_boxed_logger(Box::new(otel_log_appender)).unwrap();
-///    log::set_max_level(Level::Info.to_level_filter());
-///    // Emit logs using macros from the log crate.
-///    error!("hello from {}. My price is {}", "apple", 2.99);
-///    warn!("warn!");
-///    info!("test log!");
+///    // Setup Log Appenders and emit logs. (Not shown here)
 ///    logger_provider.force_flush();
 ///    let emitted_logs = exporter.get_emitted_logs().unwrap();
 ///    for log in emitted_logs {
