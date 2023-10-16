6b10345b5e663a9f39953982523df2396873cae6
Cijo Thomas <cijo.thomas@gmail.com>
2023-10-16 14:40:12 -0700

    tests added

--- opentelemetry-sdk/src/trace/span.rs
+++ opentelemetry-sdk/src/trace/span.rs
@@ -251,11 +251,12 @@ mod tests {
     use super::*;
     use crate::testing::trace::NoopSpanExporter;
     use crate::trace::span_limit::{
-        DEFAULT_MAX_ATTRIBUTES_PER_EVENT, DEFAULT_MAX_ATTRIBUTES_PER_LINK,
+        DEFAULT_MAX_ATTRIBUTES_PER_EVENT, DEFAULT_MAX_ATTRIBUTES_PER_LINK, DEFAULT_MAX_ATTRIBUTES_PER_SPAN,
     };
-    use opentelemetry::trace::{Link, TraceFlags, TraceId, Tracer};
+    use opentelemetry::trace::{Link, TraceFlags, TraceId, Tracer, SpanBuilder};
     use opentelemetry::{trace::Span as _, trace::TracerProvider, KeyValue};
     use std::time::Duration;
+    use std::vec;
 
     fn init() -> (crate::trace::Tracer, SpanData) {
         let provider = crate::trace::TracerProvider::default();
@@ -502,6 +503,37 @@ mod tests {
         let mut span = create_span();
         span.end();
         assert!(!span.is_recording());
+    }
+
+    #[test]
+    fn exceed_span_attributes_limit() {
+        let exporter = NoopSpanExporter::new();
+        let provider_builder =
+            crate::trace::TracerProvider::builder().with_simple_exporter(exporter);
+        let provider = provider_builder.build();
+        let tracer = provider.tracer("opentelemetry-test");
+
+        let mut initial_attributes = Vec::new();   
+        let mut expected_dropped_count = 1;
+        for i in 0..(DEFAULT_MAX_ATTRIBUTES_PER_SPAN + 1) {
+            initial_attributes.push(KeyValue::new(format!("key {}", i), i.to_string()))
+        }
+        let span_builder = SpanBuilder::from_name("test_span").with_attributes(initial_attributes);
+
+        let mut span = tracer.build(span_builder);
+        expected_dropped_count += 1;
+        span.set_attribute(KeyValue::new("key3", "value3"));
+
+        expected_dropped_count += 2;
+        let span_attributes_after_creation = vec![
+            KeyValue::new("foo", "1"),
+            KeyValue::new("bar", "2"),
+        ];
+        span.set_attributes(span_attributes_after_creation);
+
+        let actual_span = span.data.clone().expect("span data should not be empty as we already set it before");
+        assert_eq!(actual_span.attributes.len(), DEFAULT_MAX_ATTRIBUTES_PER_SPAN as usize, "Span attributes should be truncated to the max limit");
+        assert_eq!(actual_span.dropped_attributes_count, expected_dropped_count, "Dropped count should match the actual count of attributes dropped");
     }
 
     #[test]
