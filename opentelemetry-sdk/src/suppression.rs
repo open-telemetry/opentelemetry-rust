bc9da9b8d30fabb1fcb7a4514a884fd9e0a57b0e
Lalit <labhas@microsoft.com>
2023-10-30 23:26:09 -0700

    use task-local storage

--- opentelemetry-sdk/src/suppression.rs
+++ opentelemetry-sdk/src/suppression.rs
@@ -3,47 +3,62 @@ use std::cell::RefCell;
 use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context as TaskContext, Poll};
-use std::thread;
-use tokio::task;
+use tokio::task_local;
 
-thread_local! {
-    static SUPPRESSION_ENABLED: RefCell<bool> = RefCell::new(false)
+task_local! {
+    static SUPPRESSION_ENABLED: RefCell<bool>
 }
 
 #[derive(Clone, Copy, Debug)]
+///suppression context
 pub struct SuppressionContext;
 
 impl SuppressionContext {
+    ///current
     pub fn current() -> Self {
         SuppressionContext
     }
 
+    ///attach
     pub fn attach(self) -> Guard {
-        let was_suppressed = SUPPRESSION_ENABLED.with(|suppressed| {
-            let was_suppressed = *suppressed.borrow();
-            *suppressed.borrow_mut() = true;
-            was_suppressed
-        });
+        let was_suppressed = SUPPRESSION_ENABLED
+            .try_with(|suppressed| {
+                let was_suppressed = *suppressed.borrow();
+                *suppressed.borrow_mut() = true;
+                was_suppressed
+            })
+            .unwrap_or_else(|_| {
+                SUPPRESSION_ENABLED.with(|suppressed| {
+                    *suppressed.borrow_mut() = true;
+                    false // was_suppressed is false since it was not previously initialized
+                })
+            });
         Guard { was_suppressed }
     }
-
+    ///is_suppressed
     pub fn is_suppressed() -> bool {
-        let is_suppressed = SUPPRESSION_ENABLED.with(|suppressed| *suppressed.borrow());
-        is_suppressed
+        SUPPRESSION_ENABLED
+            .try_with(|suppressed| *suppressed.borrow())
+            .unwrap_or(false)
     }
 }
 
+/// Guard
+#[derive(Debug)]
 pub struct Guard {
     was_suppressed: bool,
 }
 
 impl Drop for Guard {
     fn drop(&mut self) {
-        SUPPRESSION_ENABLED.with(|suppressed| *suppressed.borrow_mut() = self.was_suppressed);
+        let _ = SUPPRESSION_ENABLED.try_with(|suppressed| {
+            *suppressed.borrow_mut() = self.was_suppressed;
+        });
     }
 }
 
 pin_project! {
+    /// WithSuppContext
     #[derive(Clone, Debug)]
     pub struct WithSuppContext<T> {
         #[pin]
@@ -62,6 +77,20 @@ impl<T: Future> Future for WithSuppContext<T> {
     }
 }
 
+/// with_init_suppression
+pub async fn with_init_suppression<F, Fut>(func: F)
+where
+    F: FnOnce() -> Fut,
+    Fut: Future<Output = ()>,
+{
+    SUPPRESSION_ENABLED
+        .scope(RefCell::new(true), async {
+            func().await;
+        })
+        .await;
+}
+
+/// with_suppression
 pub fn with_suppression<T>(inner: T) -> WithSuppContext<T> {
     WithSuppContext {
         inner,
@@ -69,6 +98,7 @@ pub fn with_suppression<T>(inner: T) -> WithSuppContext<T> {
     }
 }
 
+/// is_suppressed
 pub fn is_suppressed() -> bool {
     SuppressionContext::is_suppressed()
 }
