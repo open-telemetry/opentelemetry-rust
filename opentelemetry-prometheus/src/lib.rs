d4647e2f68090d2c2afd65395521c380fc3a2b12
Shaun Cox <shaunco@microsoft.com>
2023-11-06 09:07:38 -0600

    fix prometheus doc test

--- opentelemetry-prometheus/src/lib.rs
+++ opentelemetry-prometheus/src/lib.rs
@@ -3,7 +3,7 @@
 //! [Prometheus]: https://prometheus.io
 //!
 //! ```
-//! use opentelemetry::{metrics::MeterProvider as _, KeyValue};
+//! use opentelemetry::{metrics::MeterProvider, KeyValue};
 //! use opentelemetry_sdk::metrics::SdkMeterProvider;
 //! use prometheus::{Encoder, TextEncoder};
 //!
@@ -18,7 +18,7 @@
 //!     .build()?;
 //!
 //! // set up a meter meter to create instruments
-//! let provider = MeterProvider::builder().with_reader(exporter).build();
+//! let provider = SdkMeterProvider::builder().with_reader(exporter).build();
 //! let meter = provider.meter("my-app");
 //!
 //! // Use two instruments
