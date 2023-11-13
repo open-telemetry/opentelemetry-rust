e493e95a3b01b8e68881edf201902d6ec1aeed1d
Zhongyang Wu <zhongyang.wu@outlook.com>
2023-11-12 22:06:46 -0800

    fix(api): move composite propagator to API

--- opentelemetry/src/propagation/composite.rs
+++ opentelemetry/src/propagation/composite.rs
@@ -1,10 +1,17 @@
-use opentelemetry::{
+//! # Composite Propagator
+//!
+//! A utility over multiple `Propagator`s to group multiple Propagators from different cross-cutting
+//! concerns in order to leverage them as a single entity.
+//!
+//! Each composite Propagator will implement a specific Propagator type, such as TextMapPropagator,
+//! as different Propagator types will likely operate on different data types.
+use crate::{
     propagation::{text_map_propagator::FieldIter, Extractor, Injector, TextMapPropagator},
     Context,
 };
 use std::collections::HashSet;
 
-/// Composite propagator
+/// Composite propagator for [`TextMapPropagator`]s.
 ///
 /// A propagator that chains multiple [`TextMapPropagator`] propagators together,
 /// injecting or extracting by their respective HTTP header names.
@@ -19,12 +26,13 @@ use std::collections::HashSet;
 /// ```
 /// use opentelemetry::{
 ///     baggage::BaggageExt,
-///     propagation::TextMapPropagator,
+///     propagation::{TextMapPropagator, TextMapCompositePropagator},
+///
 ///     trace::{TraceContextExt, Tracer, TracerProvider},
 ///     Context, KeyValue,
 /// };
 /// use opentelemetry_sdk::propagation::{
-///     BaggagePropagator, TextMapCompositePropagator, TraceContextPropagator,
+///     BaggagePropagator, TraceContextPropagator,
 /// };
 /// use opentelemetry_sdk::trace as sdktrace;
 /// use std::collections::HashMap;
@@ -67,7 +75,7 @@ pub struct TextMapCompositePropagator {
 impl TextMapCompositePropagator {
     /// Constructs a new propagator out of instances of [`TextMapPropagator`].
     ///
-    /// [`TextMapPropagator`]: opentelemetry::propagation::TextMapPropagator
+    /// [`TextMapPropagator`]: TextMapPropagator
     pub fn new(propagators: Vec<Box<dyn TextMapPropagator + Send + Sync>>) -> Self {
         let mut fields = HashSet::new();
         for propagator in &propagators {
@@ -107,31 +115,31 @@ impl TextMapPropagator for TextMapCompositePropagator {
     }
 }
 
-#[cfg(all(test, feature = "testing", feature = "trace"))]
+#[cfg(test)]
 mod tests {
-    use crate::propagation::{TextMapCompositePropagator, TraceContextPropagator};
+    use crate::baggage::BaggageExt;
+    use crate::propagation::TextMapCompositePropagator;
     use crate::testing::trace::TestSpan;
-    use opentelemetry::{
+    use crate::{
         propagation::{text_map_propagator::FieldIter, Extractor, Injector, TextMapPropagator},
         trace::{SpanContext, SpanId, TraceContextExt, TraceFlags, TraceId, TraceState},
-        Context,
+        Context, KeyValue,
     };
     use std::collections::HashMap;
-    use std::str::FromStr;
 
-    /// Dummy propagator for testing
-    ///
-    /// The format we are using is {trace id(in base10 u128)}-{span id(in base10 u64)}-{flag(in u8)}
+    /// A test propagator that injects and extracts a single header.
     #[derive(Debug)]
     struct TestPropagator {
-        fields: [String; 1],
+        header: &'static str,
+        fields: Vec<String>, // used by fields method
     }
 
     impl TestPropagator {
         #[allow(unreachable_pub)]
-        pub fn new() -> Self {
+        pub fn new(header: &'static str) -> Self {
             TestPropagator {
-                fields: ["testheader".to_string()],
+                header,
+                fields: vec![header.to_string()],
             }
         }
     }
@@ -140,69 +148,60 @@ mod tests {
         fn inject_context(&self, cx: &Context, injector: &mut dyn Injector) {
             let span = cx.span();
             let span_context = span.span_context();
-            injector.set(
-                "testheader",
-                format!(
-                    "{:x}-{:x}-{:02x}",
-                    span_context.trace_id(),
-                    span_context.span_id(),
-                    span_context.trace_flags()
-                ),
-            )
+            match self.header {
+                "span-id" => injector.set(self.header, format!("{:x}", span_context.span_id())),
+                "baggage" => injector.set(self.header, cx.baggage().to_string()),
+                _ => {}
+            }
         }
 
         fn extract_with_context(&self, cx: &Context, extractor: &dyn Extractor) -> Context {
-            let span = if let Some(val) = extractor.get("testheader") {
-                let parts = val.split_terminator('-').collect::<Vec<&str>>();
-                if parts.len() != 3 {
-                    SpanContext::empty_context()
-                } else {
-                    SpanContext::new(
-                        TraceId::from_u128(u128::from_str(parts[0]).unwrap_or(0)),
-                        SpanId::from_u64(u64::from_str(parts[1]).unwrap_or(0)),
-                        TraceFlags::new(u8::from_str(parts[2]).unwrap_or(0)),
-                        true,
-                        TraceState::default(),
-                    )
-                }
-            } else {
-                SpanContext::empty_context()
-            };
-
-            cx.with_remote_span_context(span)
+            match (self.header, extractor.get(self.header)) {
+                ("span-id", Some(val)) => cx.with_remote_span_context(SpanContext::new(
+                    TraceId::from_u128(1),
+                    SpanId::from_u64(u64::from_str_radix(val, 16).unwrap()),
+                    TraceFlags::default(),
+                    false,
+                    TraceState::default(),
+                )),
+                ("baggage", Some(_)) => cx.with_baggage(vec![KeyValue::new("baggagekey", "value")]),
+                _ => cx.clone(),
+            }
         }
 
         fn fields(&self) -> FieldIter<'_> {
-            FieldIter::new(&self.fields)
+            FieldIter::new(self.fields.as_slice())
         }
     }
 
+    fn setup() -> Context {
+        let mut cx = Context::default();
+        cx = cx.with_span(TestSpan(SpanContext::new(
+            TraceId::from_u128(1),
+            SpanId::from_u64(11),
+            TraceFlags::default(),
+            true,
+            TraceState::default(),
+        )));
+        // setup for baggage propagator
+        cx = cx.with_baggage(vec![KeyValue::new("baggagekey", "value")]);
+        return cx;
+    }
+
     fn test_data() -> Vec<(&'static str, &'static str)> {
-        vec![
-            ("testheader", "1-1-00"),
-            (
-                "traceparent",
-                "00-00000000000000000000000000000001-0000000000000001-00",
-            ),
-        ]
+        vec![("span-id", "b"), ("baggage", "baggagekey=value")]
     }
 
     #[test]
     fn zero_propogators_are_noop() {
+        // setup
         let composite_propagator = TextMapCompositePropagator::new(vec![]);
+        let cx = setup();
 
-        let cx = Context::default().with_span(TestSpan(SpanContext::new(
-            TraceId::from_u128(1),
-            SpanId::from_u64(1),
-            TraceFlags::default(),
-            false,
-            TraceState::default(),
-        )));
         let mut injector = HashMap::new();
         composite_propagator.inject_context(&cx, &mut injector);
 
         assert_eq!(injector.len(), 0);
-
         for (header_name, header_value) in test_data() {
             let mut extractor = HashMap::new();
             extractor.insert(header_name.to_string(), header_value.to_string());
@@ -218,20 +217,12 @@ mod tests {
 
     #[test]
     fn inject_multiple_propagators() {
-        let test_propagator = TestPropagator::new();
-        let trace_context = TraceContextPropagator::new();
         let composite_propagator = TextMapCompositePropagator::new(vec![
-            Box::new(test_propagator),
-            Box::new(trace_context),
+            Box::new(TestPropagator::new("span-id")),
+            Box::new(TestPropagator::new("baggage")),
         ]);
 
-        let cx = Context::default().with_span(TestSpan(SpanContext::new(
-            TraceId::from_u128(1),
-            SpanId::from_u64(1),
-            TraceFlags::default(),
-            false,
-            TraceState::default(),
-        )));
+        let cx = setup();
         let mut injector = HashMap::new();
         composite_propagator.inject_context(&cx, &mut injector);
 
@@ -242,64 +233,52 @@ mod tests {
 
     #[test]
     fn extract_multiple_propagators() {
-        let test_propagator = TestPropagator::new();
-        let trace_context = TraceContextPropagator::new();
         let composite_propagator = TextMapCompositePropagator::new(vec![
-            Box::new(test_propagator),
-            Box::new(trace_context),
+            Box::new(TestPropagator::new("span-id")),
+            Box::new(TestPropagator::new("baggage")),
         ]);
 
+        let mut extractor = HashMap::new();
         for (header_name, header_value) in test_data() {
-            let mut extractor = HashMap::new();
             extractor.insert(header_name.to_string(), header_value.to_string());
-            assert_eq!(
-                composite_propagator
-                    .extract(&extractor)
-                    .span()
-                    .span_context(),
-                &SpanContext::new(
-                    TraceId::from_u128(1),
-                    SpanId::from_u64(1),
-                    TraceFlags::default(),
-                    true,
-                    TraceState::default(),
-                )
-            );
         }
+        let cx = composite_propagator.extract(&extractor);
+        assert_eq!(
+            cx.span().span_context(),
+            &SpanContext::new(
+                TraceId::from_u128(1),
+                SpanId::from_u64(11),
+                TraceFlags::default(),
+                false,
+                TraceState::default(),
+            )
+        );
+        assert_eq!(cx.baggage().to_string(), "baggagekey=value",);
     }
 
     #[test]
     fn test_get_fields() {
-        let test_propagator = TestPropagator::new();
-        let b3_fields = test_propagator
-            .fields()
-            .map(|s| s.to_string())
-            .collect::<Vec<String>>();
+        let test_cases = vec![
+            // name, header_name, expected_result
+            ("single propagator", vec!["span-id"], vec!["span-id"]),
+            ("multiple propagators with order", vec!["span-id", "baggage"], vec!["span-id", "baggage"]),
+        ];
 
-        let trace_context = TraceContextPropagator::new();
-        let trace_context_fields = trace_context
-            .fields()
-            .map(|s| s.to_string())
-            .collect::<Vec<String>>();
+        for test_case in test_cases {
+            let test_propagators = test_case.1.into_iter().map(|name | {
+                Box::new(TestPropagator::new(name)) as Box<dyn TextMapPropagator + Send + Sync>
+            }).collect();
 
-        let composite_propagator = TextMapCompositePropagator::new(vec![
-            Box::new(test_propagator),
-            Box::new(trace_context),
-        ]);
+            let composite_propagator = TextMapCompositePropagator::new(
+                test_propagators
+            );
 
-        let mut fields = composite_propagator
-            .fields()
-            .map(|s| s.to_string())
-            .collect::<Vec<String>>();
-        fields.sort();
+            let fields = composite_propagator
+                .fields()
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>();
 
-        let mut expected = vec![b3_fields, trace_context_fields]
-            .into_iter()
-            .flatten()
-            .collect::<Vec<String>>();
-        expected.sort();
-        expected.dedup();
-
-        assert_eq!(fields, expected);
+            assert_eq!(fields, test_case.2);
+        }
     }
 }
--- opentelemetry/src/propagation/mod.rs
+++ opentelemetry/src/propagation/mod.rs
@@ -1,170 +1,27 @@
 //! # OpenTelemetry Propagator interface
+//! Cross-cutting concerns send their state to the next process using Propagators, which are defined
+//! as objects used to read and write context data to and from messages exchanged by the applications.
 //!
-//! Propagators API consists of two main formats:
+//! `Propagator`s leverage the [`Context`] to inject and extract data for each cross-cutting concern,
+//! such as [`TraceContext`] and [`Baggage`].
 //!
-//! - `BinaryFormat` is used to serialize and deserialize a value
-//! into a binary representation.
-//! - `TextMapFormat` is used to inject and extract a value as
-//! text into injectors and extractors that travel in-band across process boundaries.
+//! The Propagators API is expected to be leveraged by users writing instrumentation libraries.
 //!
-//! Deserializing must set `is_remote` to true on the returned
-//! `SpanContext`.
+//! Currently, the following `Propagator` types are supported:
+//! -  [`TextMapPropagator`], inject values into and extracts values from carriers as string key/value pairs
 //!
-//! ## Binary Format
+//! A binary Propagator type will be added in
+//! the future, See [tracking issues](https://github.com/open-telemetry/opentelemetry-specification/issues/437)).
 //!
-//! `BinaryFormat` is a formatter to serialize and deserialize a value
-//! into a binary format.
-//!
-//! `BinaryFormat` MUST expose the APIs that serializes values into bytes,
-//! and deserializes values from bytes.
-//!
-//! ### ToBytes
-//!
-//! Serializes the given value into the on-the-wire representation.
-//!
-//! Required arguments:
-//!
-//! - the value to serialize, can be `SpanContext` or `DistributedContext`.
-//!
-//! Returns the on-the-wire byte representation of the value.
-//!
-//! ### FromBytes
-//!
-//! Creates a value from the given on-the-wire encoded representation.
-//!
-//! If the value could not be parsed, the underlying implementation
-//! SHOULD decide to return ether an empty value, an invalid value, or
-//! a valid value.
-//!
-//! Required arguments:
-//!
-//! - on-the-wire byte representation of the value.
-//!
-//! Returns a value deserialized from bytes.
-//!
-//! ## TextMap Format
-//!
-//! `TextMapFormat` is a formatter that injects and extracts a value
-//! as text into injectors and extractors that travel in-band across process boundaries.
-//!
-//! Encoding is expected to conform to the HTTP Header Field semantics.
-//! Values are often encoded as RPC/HTTP request headers.
-//!
-//! The carrier of propagated data on both the client (injector) and
-//! server (extractor) side is usually a http request. Propagation is
-//! usually implemented via library-specific request interceptors, where
-//! the client-side injects values and the server-side extracts them.
-//!
-//! `TextMapFormat` MUST expose the APIs that injects values into injectors,
-//! and extracts values from extractors.
-//!
-//! ### Fields
-//!
-//! The propagation fields defined. If your injector is reused, you should
-//! delete the fields here before calling `inject`.
-//!
-//! For example, if the injector is a single-use or immutable request object,
-//! you don't need to clear fields as they couldn't have been set before.
-//! If it is a mutable, retryable object, successive calls should clear
-//! these fields first.
-//!
-//! The use cases of this are:
-//!
-//! - allow pre-allocation of fields, especially in systems like gRPC
-//! Metadata
-//! - allow a single-pass over an iterator
-//!
-//! Returns list of fields that will be used by this formatter.
-//!
-//! ### Inject
-//!
-//! Injects the value downstream. For example, as http headers.
-//!
-//! Required arguments:
-//!
-//! - the `SpanContext` to be injected.
-//! - the injector that holds propagation fields. For example, an outgoing
-//! message or http request.
-//! - the `Setter` invoked for each propagation key to add or remove.
-//!
-//! #### Setter argument
-//!
-//! Setter is an argument in `Inject` that puts value into given field.
-//!
-//! `Setter` allows a `TextMapFormat` to set propagated fields into a
-//! injector.
-//!
-//! `Setter` MUST be stateless and allowed to be saved as a constant to
-//! avoid runtime allocations. One of the ways to implement it is `Setter`
-//! class with `Put` method as described below.
-//!
-//! ##### Put
-//!
-//! Replaces a propagated field with the given value.
-//!
-//! Required arguments:
-//!
-//! - the injector holds propagation fields. For example, an outgoing message
-//! or http request.
-//! - the key of the field.
-//! - the value of the field.
-//!
-//! The implementation SHOULD preserve casing (e.g. it should not transform
-//! `Content-Type` to `content-type`) if the used protocol is case insensitive,
-//! otherwise it MUST preserve casing.
-//!
-//! ### Extract
-//!
-//! Extracts the value from upstream. For example, as http headers.
-//!
-//! If the value could not be parsed, the underlying implementation will
-//! decide to return an object representing either an empty value, an invalid
-//! value, or a valid value.
-//!
-//! Required arguments:
-//!
-//! - the extractor holds propagation fields. For example, an outgoing message
-//! or http request.
-//! - the instance of `Getter` invoked for each propagation key to get.
-//!
-//! Returns the non-null extracted value.
-//!
-//! #### Getter argument
-//!
-//! Getter is an argument in `Extract` that get value from given field
-//!
-//! `Getter` allows a `TextMapFormat` to read propagated fields from a
-//! extractor.
-//!
-//! `Getter` MUST be stateless and allowed to be saved as a constant to avoid
-//! runtime allocations. One of the ways to implement it is `Getter` class
-//! with `Get` method as described below.
-//!
-//! ##### Get
-//!
-//! The Get function MUST return the first value of the given propagation
-//! key or return `None` if the key doesn't exist.
-//!
-//! Required arguments:
-//!
-//! - the extractor of propagation fields, such as an HTTP request.
-//! - the key of the field.
-//!
-//! The `get` function is responsible for handling case sensitivity. If
-//! the getter is intended to work with an HTTP request object, the getter
-//! MUST be case insensitive. To improve compatibility with other text-based
-//! protocols, text format implementations MUST ensure to always use the
-//! canonical casing for their attributes. NOTE: Canonical casing for HTTP
-//! headers is usually title case (e.g. `Content-Type` instead of `content-type`).
-//!
-//! ##### Keys
-//!
-//! The Keys function returns a vector of the propagation keys.
+//! `Propagator`s uses [`Injector`] and [`Extractor`] to read and write context data to and from messages.
+//! Each specific Propagator type defines its expected carrier type, such as a string map or a byte array.
 //!
 use std::collections::HashMap;
 
+pub mod composite;
 pub mod text_map_propagator;
 
+pub use composite::TextMapCompositePropagator;
 pub use text_map_propagator::TextMapPropagator;
 
 /// Injector provides an interface for adding fields from an underlying struct like `HashMap`
