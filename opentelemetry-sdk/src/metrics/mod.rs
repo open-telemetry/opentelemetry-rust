bf988b001abf3260833451a6bfcf72bea349cf96
Cijo Thomas <cijo.thomas@gmail.com>
2023-11-17 11:46:39 -0800

    Attempt to write metric tests with inmemoryexp

--- opentelemetry-sdk/src/metrics/mod.rs
+++ opentelemetry-sdk/src/metrics/mod.rs
@@ -57,3 +57,86 @@ pub use meter_provider::*;
 pub use periodic_reader::*;
 pub use pipeline::Pipeline;
 pub use view::*;
+
+#[cfg(all(test, feature = "testing"))]
+mod tests {
+    use super::*;
+    use crate::{runtime, testing::metrics::InMemoryMetricsExporter};
+    use opentelemetry::{
+        metrics::{MeterProvider as _, Unit},
+        KeyValue,
+    };
+    use tokio::time::{sleep, Duration};
+
+    #[tokio::test]
+    async fn counter_aggregation() {
+        // Run this test with stdout enabled to see output.
+        // cargo test counter --features=metrics,testing -- --nocapture
+        
+        // Arrange
+        let exporter = InMemoryMetricsExporter::default();
+        let reader = PeriodicReader::builder(exporter.clone(), runtime::Tokio).build();
+        let meter_provider = SdkMeterProvider::builder().with_reader(reader).build();
+        
+        // Act
+        let meter = meter_provider.meter("test");
+        let counter = meter
+            .u64_counter("my_counter")
+            .with_unit(Unit::new("my_unit"))
+            .init();
+        counter.add(1, &[KeyValue::new("key1", "value1")]);
+        counter.add(1, &[KeyValue::new("key1", "value1")]);
+        counter.add(1, &[KeyValue::new("key1", "value1")]);
+        counter.add(1, &[KeyValue::new("key1", "value1")]);
+        counter.add(1, &[KeyValue::new("key1", "value1")]);
+
+        counter.add(1, &[KeyValue::new("key1", "value2")]);
+        counter.add(1, &[KeyValue::new("key1", "value2")]);
+        counter.add(1, &[KeyValue::new("key1", "value2")]);
+
+        // TODO: Unable to get this to work, as
+        // flush is taking forever.
+        // println!("flush started");
+        // meter_provider.force_flush().unwrap();
+        // println!("flush finished");
+        // The sleep is temporary workaround to
+        // unblock progress on writing tests like
+        // this.
+        sleep(Duration::from_millis(100)).await;
+
+        // Assert
+        let resource_metrics = exporter
+            .get_finished_metrics()
+            .expect("metrics are expected to be exported.");
+        assert_eq!(resource_metrics.len(), 1);
+        let metric = &resource_metrics[0].scope_metrics[0].metrics[0];
+        assert_eq!(metric.name, "my_counter");
+        assert_eq!(metric.unit.as_str(), "my_unit");
+        let sum = metric
+            .data
+            .as_any()
+            .downcast_ref::<data::Sum<u64>>()
+            .expect("Sum aggregation expected");
+
+        // Expecting 2 time-series.
+        assert_eq!(sum.data_points.len(), 2);
+
+        // TODO: Don't think the order is guaranteed.
+        // Need to make it easy to write unit tests.s
+        let data_point1 = &sum.data_points[0];
+        data_point1
+            .attributes
+            .iter()
+            .find(|(k, v)| k.as_str() == "key1" && v.as_str() == "value1")
+            .expect("kvp expected");
+        assert_eq!(data_point1.value, 5);
+
+        let data_point1 = &sum.data_points[1];
+        data_point1
+            .attributes
+            .iter()
+            .find(|(k, v)| k.as_str() == "key1" && v.as_str() == "value2")
+            .expect("kvp expected");
+        assert_eq!(data_point1.value, 3);
+    }
+}
