32f26bd731ce1a4c3fedb6ca185212ec6a217e68
Cijo Thomas <cijo.thomas@gmail.com>
2023-11-22 14:32:59 -0800

    map is fine

--- opentelemetry-sdk/src/attributes/set.rs
+++ opentelemetry-sdk/src/attributes/set.rs
@@ -111,7 +111,7 @@ impl From<&[KeyValue]> for AttributeSet {
         let mut vec = values
             .iter()
             .rev()
-            .filter_map(|kv| Some(HashKeyValue(kv.clone())))
+            .map(|kv| HashKeyValue(kv.clone()))
             .collect::<Vec<_>>();
         vec.sort_unstable();
 
