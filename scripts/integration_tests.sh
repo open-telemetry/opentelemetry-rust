ba6042349ec0cd68466b6fe986b6c53f60cfb2e9
Zhongyang Wu <zhongyang.wu@outlook.com>
2024-01-16 00:27:43 -0800

    add into integration test script

--- scripts/integration_tests.sh
+++ scripts/integration_tests.sh
@@ -1,3 +1,5 @@
 COMPOSE_FILE=./opentelemetry-jaeger/tests/docker-compose.yaml
 docker-compose -f $COMPOSE_FILE down -v &&
 docker-compose -f $COMPOSE_FILE up --build --abort-on-container-exit --exit-code-from opentelemetry-jaeger
+
+cargo test ./opentelemetry-otlp/tests/integration_test/tests
