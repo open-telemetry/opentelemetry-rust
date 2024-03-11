6f2ace173a535c3acbf2a3b046137be395e62c09
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-03-11 13:48:48 -0700

    remove unused package

--- opentelemetry/benches/noop_metrics.rs
+++ opentelemetry/benches/noop_metrics.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use opentelemetry::{
     metrics::{noop::NoopMeterProvider, Counter, MeterProvider as _},
     KeyValue,
