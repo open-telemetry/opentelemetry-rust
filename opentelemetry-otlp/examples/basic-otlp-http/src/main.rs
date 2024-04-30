c11994a0c705c947d982784455df4ae76317a721
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-04-30 11:05:20 -0700

    Update opentelemetry-otlp/examples/basic-otlp-http/src/main.rs
    Co-authored-by: Zhongyang Wu <zhongyang.wu@outlook.com>

--- opentelemetry-otlp/examples/basic-otlp-http/src/main.rs
+++ opentelemetry-otlp/examples/basic-otlp-http/src/main.rs
@@ -76,6 +76,7 @@ static COMMON_ATTRIBUTES: Lazy<[KeyValue; 4]> = Lazy::new(|| {
 async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     let _ = init_tracer()?;
     let _ = init_metrics()?;
+    // Opentelemetry will not provide a global API to manage the logger provider. Application users must manage the lifecycle of the logger provider on their own. Dropping logger providers will disable log emitting.
     let logger_provider = init_logs().unwrap();
 
     let tracer = global::tracer("ex.com/basic");
