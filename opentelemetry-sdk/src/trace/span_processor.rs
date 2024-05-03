597b7fd6bb9c7e056dc1a2b575b988565d44ab17
Utkarsh Umesan Pillai <66651184+utpilla@users.noreply.github.com>
2024-05-02 17:56:01 -0700

    Fix lint

--- opentelemetry-sdk/src/trace/span_processor.rs
+++ opentelemetry-sdk/src/trace/span_processor.rs
@@ -399,7 +399,7 @@ impl<R: RuntimeChannel> BatchSpanProcessorInternal<R> {
         })
     }
 
-    async fn run(mut self, mut messages: impl Stream<Item = BatchMessage> + Unpin + FusedStream) {
+    async fn run(mut self, mut messages: impl Unpin + FusedStream<Item = BatchMessage>) {
         loop {
             select! {
                 // FuturesUnordered implements Fuse intelligently such that it
