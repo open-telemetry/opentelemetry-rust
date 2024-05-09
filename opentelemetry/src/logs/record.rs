900f2004499fe402bffee1882c200aff4bd90560
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-05-09 09:45:20 -0700

    default implemention for LogRecord::set_event_name

--- opentelemetry/src/logs/record.rs
+++ opentelemetry/src/logs/record.rs
@@ -4,9 +4,9 @@ use std::{borrow::Cow, collections::HashMap, time::SystemTime};
 /// SDK implemented trait for managing log records
 pub trait LogRecord {
     /// Sets the `event_name` of a record
-    fn set_event_name<T>(&mut self, name: T)
+    fn set_event_name<T>(&mut self, _name: T)
     where
-        T: Into<Cow<'static, str>>;
+        T: Into<Cow<'static, str>> {}
 
     /// Sets the time when the event occurred measured by the origin clock, i.e. the time at the source.
     fn set_timestamp(&mut self, timestamp: SystemTime);
