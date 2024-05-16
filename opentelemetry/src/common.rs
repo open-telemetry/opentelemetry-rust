5c763c3f497ebeab2fa1f853876f812e4004ca6c
Cijo Thomas <cijo.thomas@gmail.com>
2024-05-15 18:11:05 -0700

    more test

--- opentelemetry/src/common.rs
+++ opentelemetry/src/common.rs
@@ -672,6 +672,10 @@ mod tests {
         let kv2 = KeyValue::new("key", std::f64::INFINITY);
         assert_eq!(kv1, kv2);
 
+        let kv1 = KeyValue::new("key", std::f64::NEG_INFINITY);
+        let kv2 = KeyValue::new("key", std::f64::NEG_INFINITY);
+        assert_eq!(kv1, kv2);
+
         let mut rng = rand::thread_rng();
 
         for _ in 0..100 {
@@ -692,6 +696,10 @@ mod tests {
         let kv2 = KeyValue::new("key", 1.001);
         assert_eq!(hash_helper(&kv1), hash_helper(&kv2));
 
+        let kv1 = KeyValue::new("key", 1.001);
+        let kv2 = KeyValue::new("key", 1.002);
+        assert_ne!(hash_helper(&kv1), hash_helper(&kv2));
+
         let kv1 = KeyValue::new("key", std::f64::NAN);
         let kv2 = KeyValue::new("key", std::f64::NAN);
         assert_eq!(hash_helper(&kv1), hash_helper(&kv2));
@@ -707,6 +715,23 @@ mod tests {
             let kv1 = KeyValue::new("key", random_value);
             let kv2 = KeyValue::new("key", random_value);
             assert_eq!(hash_helper(&kv1), hash_helper(&kv2));
+        }
+    }
+
+    #[test]
+    fn hash_kv_order() {
+        let kv1 = KeyValue::new("a", 1.0);
+        let kv2 = KeyValue::new("b", 1.0);
+        assert!(kv1 < kv2);
+
+        let float_vals = [
+            0.0, 1.0, -1.0, std::f64::INFINITY, std::f64::NEG_INFINITY, std::f64::NAN, std::f64::MIN, std::f64::MAX,
+        ];
+
+        for v in float_vals {
+            let kv1 = KeyValue::new("a", v);
+            let kv2 = KeyValue::new("b", v);
+            assert!(kv1 < kv2, "Order is solely based on key!");
         }
     }
 
