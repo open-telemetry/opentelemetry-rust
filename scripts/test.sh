f7126f1b1c95faf942ff4b16d8b96e1b8cce73c6
Cijo Thomas <cijo.thomas@gmail.com>
2024-05-24 09:48:12 -0700

    echo

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,9 @@
 
 set -eu
 
+echo "Running tests for all packages in workspace with --all-features"
 cargo test --workspace --all-features
 
 # See https://github.com/rust-lang/cargo/issues/5364
+echo "Running tests for opentelemetry package with --no-default-features"
 cargo test --manifest-path=opentelemetry/Cargo.toml --no-default-features
