48bce314d610401e7ee9a8da11f89307a57289ef
Cijo Thomas <cijo.thomas@gmail.com>
2024-05-24 09:52:15 -0700

    run ignored

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,3 +8,7 @@ cargo test --workspace --all-features
 # See https://github.com/rust-lang/cargo/issues/5364
 echo "Running tests for opentelemetry package with --no-default-features"
 cargo test --manifest-path=opentelemetry/Cargo.toml --no-default-features
+
+# Run global tracer provider test in single thread
+echo "Running global tracer provider for opentelemetry-sdk package with single thread."
+cargo test --manifest-path=opentelemetry-sdk/Cargo.toml --all-features -- --ignored --test-threads=1
