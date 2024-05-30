cbd48a7800447f40afc0b27615a2aec2ae8b0e7c
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-05-29 17:41:18 -0700

    use otel-tracing-appender

--- examples/self-diagnostics/src/main.rs
+++ examples/self-diagnostics/src/main.rs
@@ -1,22 +1,23 @@
 use opentelemetry::global::{self, set_error_handler, Error as OtelError};
 use opentelemetry::KeyValue;
+use opentelemetry_appender_tracing::layer;
+use opentelemetry_sdk::logs::LoggerProvider;
 use opentelemetry_sdk::metrics::{PeriodicReader, SdkMeterProvider};
 use opentelemetry_sdk::{runtime, Resource};
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::EnvFilter;
 
 use std::error::Error;
 use tracing::error;
 
 fn custom_error_handler(err: OtelError) {
     match err {
-        #[cfg(feature = "metrics")]
-        Error::Metric(err) => error!("OpenTelemetry metrics error occurred: {}", err),
-        #[cfg(feature = "trace")]
-        Error::Trace(err) => error!("OpenTelemetry trace error occurred: {}", err),
-        #[cfg(feature = "logs")]
-        Error::Log(err) => error!("OpenTelemetry log error occurred: {}", err),
+        OtelError::Metric(err) => error!("OpenTelemetry metrics error occurred: {}", err),
+        OtelError::Trace(err) => error!("OpenTelemetry trace error occurred: {}", err),
+        OtelError::Log(err) => error!("OpenTelemetry log error occurred: {}", err),
         OtelError::Propagation(err) => error!("OpenTelemetry propagation error occurred: {}", err),
         OtelError::Other(err_msg) => error!("OpenTelemetry error occurred: {}", err_msg),
-        _ => error!("An unknown OpenTelemetry error occurred."),
+        _ => error!("An unknown OpenTelemetry error occurred"), //won't reach here
     }
 }
 
@@ -25,8 +26,18 @@ fn init_self_diagnostics() {
     if let Err(err) = set_error_handler(custom_error_handler) {
         eprintln!("Failed to set custom error handler: {}", err);
     }
-    // TODO - use otel-tracing-subscriber or provide option to use either.
-    tracing_subscriber::fmt::init();
+    //init tracing subscriber
+    let exporter = opentelemetry_stdout::LogExporter::default();
+    let provider: LoggerProvider = LoggerProvider::builder()
+        .with_simple_exporter(exporter)
+        .build();
+    //  create filter to only allow error messages
+    let filter = EnvFilter::new("error");
+    let layer = layer::OpenTelemetryTracingBridge::new(&provider);
+    tracing_subscriber::registry()
+        .with(filter)
+        .with(layer)
+        .init();
 }
 
 fn init_meter_provider() -> opentelemetry_sdk::metrics::SdkMeterProvider {
@@ -60,9 +71,9 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     // Create a Counter Instrument.
     let counter = meter.u64_counter("my_counter").init();
 
-    // Record measurements with unique key-value pairs, and enforce the error message
-    // generation by exceeding the cardinality limit
-    for i in 0..2100 {
+    // Record measurements with unique key-value pairs to exceed the cardinality limit
+    // and trigger error message
+    for i in 0..2001 {
         counter.add(
             10,
             &[KeyValue::new(
@@ -75,6 +86,8 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     // Metrics are exported by default every 30 seconds when using stdout exporter,
     // however shutting down the MeterProvider here instantly flushes
     // the metrics, instead of waiting for the 30 sec interval.
+    meter_provider.shutdown()?;
+    // shutdown again to trigger error message.
     meter_provider.shutdown()?;
     Ok(())
 }
