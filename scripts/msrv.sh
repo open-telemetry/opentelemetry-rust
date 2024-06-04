a54db842f26c86d41909063099e6f23bac5a0fdc
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-06-04 14:48:03 -0700

    fix newlines for packages

--- scripts/msrv.sh
+++ scripts/msrv.sh
@@ -52,6 +52,7 @@ echo "checking packages: "$packages
 echo
 # Check MSRV for the packages
 for package in $packages; do
+  package=$(echo "$package" | tr -d '\r\n') # Remove any newline and carriage return characters
   echo "Running MSRV check for $package with Rust $installed_version"
   echo "Current working directory: $(pwd)"
   echo "Command: rustup run \"$installed_version\" cargo check --manifest-path=\"$package\" --all-features"
