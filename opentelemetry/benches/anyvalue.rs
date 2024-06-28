17b633d0c890d8424dda730803a9741eb6053846
Cijo Thomas <cijo.thomas@gmail.com>
2024-06-27 18:49:41 -0700

    add int too

--- opentelemetry/benches/anyvalue.rs
+++ opentelemetry/benches/anyvalue.rs
@@ -4,23 +4,37 @@ use opentelemetry::{logs::AnyValue, Value};
 // Run this benchmark with:
 // cargo bench --bench anyvalue
 // Results:
-// CreateOTelValue 1-2 ns
-// CreateOTelAnyValue 15 ns
+// CreateOTelValueString 1-2 ns
+// CreateOTelAnyValueString 15 ns
+// CreateOTelValueInt 1-2 ns
+// CreateOTelAnyValueInt 15 ns
 
 fn criterion_benchmark(c: &mut Criterion) {
     attributes_creation(c);
 }
 
 fn attributes_creation(c: &mut Criterion) {
-    c.bench_function("CreateOTelValue", |b| {
+    c.bench_function("CreateOTelValueString", |b| {
         b.iter(|| {
             let _v = black_box(Value::String("value1".into()));
         });
     });
 
-    c.bench_function("CreateOTelAnyValue", |b| {
+    c.bench_function("CreateOTelAnyValueString", |b| {
         b.iter(|| {
             let _v = black_box(AnyValue::String("value1".into()));
+        });
+    });
+
+    c.bench_function("CreateOTelValueInt", |b| {
+        b.iter(|| {
+            let _v = black_box(Value::I64(123));
+        });
+    });
+
+    c.bench_function("CreateOTelAnyValueInt", |b| {
+        b.iter(|| {
+            let _v = black_box(AnyValue::Int(123));
         });
     });
 }
