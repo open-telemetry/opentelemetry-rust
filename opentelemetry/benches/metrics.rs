32d73fd0aede9164321d810b003bd2ca2ff5a259
Utkarsh Umesan Pillai <66651184+utpilla@users.noreply.github.com>
2024-07-02 21:47:08 +0000

    Update metric benchmarks

--- opentelemetry/benches/metrics.rs
+++ opentelemetry/benches/metrics.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
 use opentelemetry::{global, metrics::Counter, KeyValue};
 
 // Run this benchmark with:
@@ -28,10 +28,10 @@ fn counter_add(c: &mut Criterion) {
             counter.add(
                 1,
                 &[
-                    KeyValue::new("attribute1", "value1"),
-                    KeyValue::new("attribute2", "value2"),
-                    KeyValue::new("attribute3", "value3"),
-                    KeyValue::new("attribute4", "value4"),
+                    black_box(KeyValue::new("attribute1", "value1")),
+                    black_box(KeyValue::new("attribute2", "value2")),
+                    black_box(KeyValue::new("attribute3", "value3")),
+                    black_box(KeyValue::new("attribute4", "value4")),
                 ],
             );
         });
@@ -51,16 +51,26 @@ fn counter_add(c: &mut Criterion) {
     });
 
     c.bench_function("Counter_AddWithDynamicAttributes", |b| {
-        b.iter(|| {
-            let kv = vec![
-                KeyValue::new("attribute1", "value1"),
-                KeyValue::new("attribute2", "value2"),
-                KeyValue::new("attribute3", "value3"),
-                KeyValue::new("attribute4", "value4"),
-            ];
+        b.iter_batched(
+            || {
+                let value1 = black_box("a".repeat(6)); // Repeat character six times to match the length of value strings used in other benchmarks
+                let value2 = black_box("b".repeat(6));
+                let value3 = black_box("c".repeat(6));
+                let value4 = black_box("d".repeat(6));
 
-            counter.add(1, &kv);
-        });
+                (value1, value2, value3, value4)
+            },
+            |values| {
+                let kv = &[
+                    KeyValue::new("attribute1", values.0),
+                    KeyValue::new("attribute2", values.1),
+                    KeyValue::new("attribute3", values.2),
+                    KeyValue::new("attribute4", values.3),
+                ];
+                
+                counter.add(1, kv);
+            },
+            BatchSize::SmallInput);
     });
 }
 
