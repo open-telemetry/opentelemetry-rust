7fee0fcfd093cea1cd012f89018c38adcc6db2cc
Lalit <labhas@microsoft.com>
2024-07-22 19:35:20 -0700

    inital commit

---
+++ opentelemetry-sdk/benches/hybrid_vec.rs
@@ -0,0 +1,80 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use opentelemetry::logs::AnyValue;
+use opentelemetry::Key;
+use opentelemetry_sdk::logs::HybridVec;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct KeyValuePair(Key, AnyValue);
+
+impl Default for KeyValuePair {
+    fn default() -> Self {
+        KeyValuePair(Key::from_static_str(""), AnyValue::String("".into()))
+    }
+}
+
+fn hybridvec_insertion_benchmark(c: &mut Criterion) {
+    c.bench_function("HybridVec Insertion", |b| {
+        b.iter(|| {
+            let mut collection = HybridVec::<KeyValuePair>::new();
+            for i in 0..8 {
+                let key = Key::from(format!("key{}", i));
+                let value = AnyValue::Int(i as i64);
+                collection.push(KeyValuePair(key, value));
+            }
+        })
+    });
+}
+
+fn vec_insertion_benchmark(c: &mut Criterion) {
+    c.bench_function("Vec Insertion", |b| {
+        b.iter(|| {
+            let mut collection = Vec::new();
+            for i in 0..8 {
+                let key = Key::from(format!("key{}", i));
+                let value = AnyValue::Int(i as i64);
+                collection.push(KeyValuePair(key, value));
+            }
+        })
+    });
+}
+
+fn hybridvec_iteration_benchmark(c: &mut Criterion) {
+    c.bench_function("HybridVec Iteration", |b| {
+        let mut collection = HybridVec::<KeyValuePair>::new();
+        for i in 0..8 {
+            let key = Key::from(format!("key{}", i));
+            let value = AnyValue::Int(i as i64);
+            collection.push(KeyValuePair(key, value));
+        }
+        b.iter(|| {
+            for item in &collection {
+                criterion::black_box(item);
+            }
+        })
+    });
+}
+
+fn vec_iteration_benchmark(c: &mut Criterion) {
+    c.bench_function("Vec Iteration", |b| {
+        let mut collection = Vec::new();
+        for i in 0..8 {
+            let key = Key::from(format!("key{}", i));
+            let value = AnyValue::Int(i as i64);
+            collection.push(KeyValuePair(key, value));
+        }
+        b.iter(|| {
+            for item in &collection {
+                criterion::black_box(item);
+            }
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    hybridvec_insertion_benchmark,
+    vec_insertion_benchmark,
+    hybridvec_iteration_benchmark,
+    vec_iteration_benchmark
+);
+criterion_main!(benches);
---
+++ opentelemetry-sdk/src/logs/hybrid_vec.rs
@@ -0,0 +1,209 @@
+use std::array::IntoIter;
+
+/// The default initial capacity for `HybridVec`.
+const DEFAULT_INITIAL_CAPACITY: usize = 10;
+
+#[derive(Debug)]
+/// A hybrid vector that starts with a fixed-size array and grows dynamically with a vector.
+pub struct HybridVec<T: Default, const INITIAL_CAPACITY: usize = DEFAULT_INITIAL_CAPACITY> {
+    initial: [T; INITIAL_CAPACITY],
+    additional: Vec<T>,
+    count: usize,
+}
+
+impl<T: Default, const INITIAL_CAPACITY: usize> HybridVec<T, INITIAL_CAPACITY> {
+    /// Creates a new `HybridVec` with the default initial capacity.
+    pub fn new() -> Self {
+        Self {
+            initial: [(); INITIAL_CAPACITY].map(|_| T::default()),
+            additional: Vec::new(),
+            count: 0,
+        }
+    }
+
+    /// Pushes a value into the `HybridVec`.
+    pub fn push(&mut self, value: T) {
+        if self.count < INITIAL_CAPACITY {
+            self.initial[self.count] = value;
+            self.count += 1;
+        } else {
+            self.additional.push(value);
+        }
+    }
+
+    /// Gets a reference to the value at the specified index.
+    pub fn get(&self, index: usize) -> Option<&T> {
+        if index < self.count {
+            Some(&self.initial[index])
+        } else if index < self.count + self.additional.len() {
+            self.additional.get(index - INITIAL_CAPACITY)
+        } else {
+            None
+        }
+    }
+
+    /// Returns the number of elements in the `HybridVec`.
+    pub fn len(&self) -> usize {
+        self.count + self.additional.len()
+    }
+}
+
+// Implement `IntoIterator` for `HybridVec`
+impl<T: Default, const INITIAL_CAPACITY: usize> IntoIterator for HybridVec<T, INITIAL_CAPACITY> {
+    type Item = T;
+    type IntoIter = HybridVecIntoIter<T, INITIAL_CAPACITY>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        HybridVecIntoIter {
+            initial_iter: self.initial.into_iter().take(self.count),
+            additional_iter: self.additional.into_iter(),
+        }
+    }
+}
+
+#[derive(Debug)]
+/// Iterator for consuming a `HybridVec`.
+pub struct HybridVecIntoIter<T: Default, const INITIAL_CAPACITY: usize> {
+    initial_iter: std::iter::Take<IntoIter<T, INITIAL_CAPACITY>>,
+    additional_iter: std::vec::IntoIter<T>,
+}
+
+impl<T: Default, const INITIAL_CAPACITY: usize> Iterator
+    for HybridVecIntoIter<T, INITIAL_CAPACITY>
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.initial_iter
+            .next()
+            .or_else(|| self.additional_iter.next())
+    }
+}
+
+// Implement `IntoIterator` for a reference to `HybridVec`
+impl<'a, T: Default + 'a, const INITIAL_CAPACITY: usize> IntoIterator
+    for &'a HybridVec<T, INITIAL_CAPACITY>
+{
+    type Item = &'a T;
+    type IntoIter = HybridVecIter<'a, T, INITIAL_CAPACITY>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        HybridVecIter {
+            initial_iter: self.initial.iter().take(self.count),
+            additional_iter: self.additional.iter(),
+        }
+    }
+}
+
+#[derive(Debug)]
+/// Iterator for referencing elements in a `HybridVec`.
+pub struct HybridVecIter<'a, T: Default, const INITIAL_CAPACITY: usize> {
+    initial_iter: std::iter::Take<std::slice::Iter<'a, T>>,
+    additional_iter: std::slice::Iter<'a, T>,
+}
+
+impl<'a, T: Default, const INITIAL_CAPACITY: usize> Iterator
+    for HybridVecIter<'a, T, INITIAL_CAPACITY>
+{
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.initial_iter
+            .next()
+            .or_else(|| self.additional_iter.next())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use opentelemetry::logs::AnyValue;
+    use opentelemetry::Key;
+
+    /// A struct to hold a key-value pair and implement `Default`.
+    #[derive(Clone, Debug, PartialEq)]
+    struct KeyValuePair(Key, AnyValue);
+
+    impl Default for KeyValuePair {
+        fn default() -> Self {
+            KeyValuePair(Key::from_static_str(""), AnyValue::String("".into()))
+        }
+    }
+
+    #[test]
+    fn test_push_and_get() {
+        let mut collection = HybridVec::<i32>::new();
+        for i in 0..15 {
+            collection.push(i);
+        }
+        for i in 0..15 {
+            assert_eq!(collection.get(i), Some(&(i as i32)));
+        }
+    }
+
+    #[test]
+    fn test_len() {
+        let mut collection = HybridVec::<i32>::new();
+        for i in 0..15 {
+            collection.push(i);
+        }
+        assert_eq!(collection.len(), 15);
+    }
+
+    #[test]
+    fn test_into_iter() {
+        let mut collection = HybridVec::<i32>::new();
+        for i in 0..15 {
+            collection.push(i);
+        }
+        let mut iter = collection.into_iter();
+        for i in 0..15 {
+            assert_eq!(iter.next(), Some(i));
+        }
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn test_ref_iter() {
+        let mut collection = HybridVec::<i32>::new();
+        for i in 0..15 {
+            collection.push(i);
+        }
+        let iter = &collection;
+        let mut count = 0;
+        for value in iter {
+            assert_eq!(*value, count);
+            count += 1;
+        }
+        assert_eq!(count, 15);
+    }
+
+    #[test]
+    fn test_key_value_pair_storage_hybridvec() {
+        let mut collection = HybridVec::<KeyValuePair>::new();
+
+        let key1 = Key::from("key1");
+        let value1 = AnyValue::String("value1".into());
+        let key2 = Key::from("key2");
+        let value2 = AnyValue::Int(42);
+
+        collection.push(KeyValuePair(key1.clone(), value1.clone()));
+        collection.push(KeyValuePair(key2.clone(), value2.clone()));
+
+        assert_eq!(
+            collection.get(0).map(|kv| (&kv.0, &kv.1)),
+            Some((&key1, &value1))
+        );
+        assert_eq!(
+            collection.get(1).map(|kv| (&kv.0, &kv.1)),
+            Some((&key2, &value2))
+        );
+        assert_eq!(collection.len(), 2);
+
+        // Test iterating over the key-value pairs
+        let mut iter = collection.into_iter();
+        assert_eq!(iter.next(), Some(KeyValuePair(key1, value1)));
+        assert_eq!(iter.next(), Some(KeyValuePair(key2, value2)));
+        assert_eq!(iter.next(), None);
+    }
+}
