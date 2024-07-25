b9a90c21183792c798be9a553afe2da959f8022b
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-07-25 05:11:59 +0000

    changes

--- opentelemetry-sdk/src/logs/growable_array.rs
+++ opentelemetry-sdk/src/logs/growable_array.rs
@@ -233,12 +233,6 @@ mod tests {
     use opentelemetry::logs::AnyValue;
     use opentelemetry::Key;
 
-    #[cfg(feature = "memory-profiling")]
-    use crate::testing::global_allocator;
-
-    #[cfg(feature = "memory-profiling")]
-    use jemalloc_ctl::{epoch, stats};
-
     /// A struct to hold a key-value pair and implement `Default`.
     #[derive(Clone, Debug, PartialEq)]
     struct KeyValuePair(Key, AnyValue);
@@ -337,59 +331,5 @@ mod tests {
 
         collection.push(15);
         assert!(collection.contains(&15));
-    }
-
-    #[cfg(feature = "memory-profiling")]
-    #[test]
-    fn test_memory_allocation_string() {
-        // Reset jemalloc epoch to refresh stats
-        let e = epoch::mib().unwrap();
-        e.advance().unwrap();
-
-        // Get memory stats before the code block
-        let allocated_before = stats::allocated::read().unwrap();
-
-        // Code block to measure: Allocate a large string
-        let large_string: String = "a".repeat(100_000);
-
-        // Refresh jemalloc stats
-        e.advance().unwrap();
-
-        // Get memory stats after the code block
-        let allocated_after = stats::allocated::read().unwrap();
-
-        // Calculate the difference
-        let allocated_diff = allocated_after - allocated_before;
-
-        // Assert or print the difference
-        println!("Memory allocated for String: {} bytes", allocated_diff);
-        assert!(allocated_diff > 0);
-    }
-
-    #[cfg(feature = "memory-profiling")]
-    #[test]
-    fn test_memory_allocation_int() {
-        // Reset jemalloc epoch to refresh stats
-        let e = epoch::mib().unwrap();
-        e.advance().unwrap();
-
-        // Get memory stats before the code block
-        let allocated_before = stats::allocated::read().unwrap();
-
-        // Code block to measure: Allocate a vector of integers
-        let vec: Vec<i32> = (0..100_000).collect();
-
-        // Refresh jemalloc stats
-        e.advance().unwrap();
-
-        // Get memory stats after the code block
-        let allocated_after = stats::allocated::read().unwrap();
-
-        // Calculate the difference
-        let allocated_diff = allocated_after - allocated_before;
-
-        // Assert or print the difference
-        println!("Memory allocated for Vec<i32>: {} bytes", allocated_diff);
-        assert!(allocated_diff > 0);
     }
 }
