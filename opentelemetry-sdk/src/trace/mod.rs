807b6a5167bf056ebecae97b5a0322c5b55d4332
Lalit <labhas@microsoft.com>
2024-07-26 18:13:32 -0700

    fix lint

--- opentelemetry-sdk/src/trace/mod.rs
+++ opentelemetry-sdk/src/trace/mod.rs
@@ -3,8 +3,7 @@
 //! The tracing SDK consist of a few main structs:
 //!
 //! * The [`Tracer`] struct which performs all tracing operations.
-//! * The [`Span`] struct with is a mutable object storing information about the
-//! current operation execution.
+//! * The [`Span`] struct with is a mutable object storing information about the current operation execution.
 //! * The [`TracerProvider`] struct which configures and produces [`Tracer`]s.
 mod config;
 mod events;
