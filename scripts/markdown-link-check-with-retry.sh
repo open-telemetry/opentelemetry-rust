c7fb1d88e26add0b9603e8d7fbbe8a0961f3e328
Lalit <labhas@microsoft.com>
2024-08-01 08:00:31 -0700

    fix

--- scripts/markdown-link-check-with-retry.sh
+++ scripts/markdown-link-check-with-retry.sh
@@ -3,20 +3,10 @@
 # this script helps to reduce sporadic link check failures by retrying at a file-by-file level
 
 retry_count=3
-ignore_dirs=("opentelemetry-proto/src/proto/opentelemetry-proto")
 
 for file in "$@"; do
-  skip=false
-  # Check if file path contains any ignore dir
-  for ignore_dir in "${ignore_dirs[@]}"; do
-    if [[ "$file" == *"$ignore_dir"* ]]; then
-      skip=true
-      break
-    fi
-  done
-  if [ "$skip" = false ]; then
-    for i in $(seq 1 $retry_count); do
-      if markdown-link-check --config "$(dirname "$0")/markdown-link-check-config.json" \
+  for i in $(seq 1 $retry_count); do
+    if markdown-link-check --config "$(dirname "$0")/markdown-link-check-config.json" \
                            "$file"; then
       break
     elif [[ $i -eq $retry_count ]]; then
@@ -24,5 +14,4 @@ for file in "$@"; do
     fi
     sleep 5
   done
-  fi
 done
