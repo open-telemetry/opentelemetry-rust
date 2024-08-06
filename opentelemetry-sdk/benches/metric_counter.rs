4c3551c1c31e09b18b2eb06b050719d0d374aa30
Cijo Thomas <cijo.thomas@gmail.com>
2024-08-06 14:53:25 -0700

    static array

--- opentelemetry-sdk/benches/metric_counter.rs
+++ opentelemetry-sdk/benches/metric_counter.rs
@@ -30,6 +30,11 @@ thread_local! {
     static CURRENT_RNG: RefCell<rngs::SmallRng> = RefCell::new(rngs::SmallRng::from_entropy());
 }
 
+static ATTRIBUTE_VALUES: [&str; 10] = [
+    "value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8", "value9",
+    "value10",
+];
+
 // Run this benchmark with:
 // cargo bench --bench metric_counter
 fn create_counter(name: &'static str) -> Counter<u64> {
@@ -39,24 +44,19 @@ fn create_counter(name: &'static str) -> Counter<u64> {
     let meter = meter_provider.meter("benchmarks");
 
     println!("Counter_Created");
-    meter.u64_counter(name).init()    
+    meter.u64_counter(name).init()
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     counter_add_sorted(c);
     counter_add_unsorted(c);
     counter_overflow(c);
+    random_generator(c);
 }
 
 fn counter_add_sorted(c: &mut Criterion) {
-    let attribute_values = [
-        "value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8", "value9",
-        "value10",
-    ];
-
     let counter = create_counter("Counter_Add_Sorted");
     c.bench_function("Counter_Add_Sorted", |b| {
-        
         b.iter(|| {
             // 4*4*10*10 = 1600 time series.
             let rands = CURRENT_RNG.with(|rng| {
@@ -75,10 +75,10 @@ fn counter_add_sorted(c: &mut Criterion) {
             counter.add(
                 1,
                 &[
-                    KeyValue::new("attribute1", attribute_values[index_first_attribute]),
-                    KeyValue::new("attribute2", attribute_values[index_second_attribute]),
-                    KeyValue::new("attribute3", attribute_values[index_third_attribute]),
-                    KeyValue::new("attribute4", attribute_values[index_fourth_attribute]),
+                    KeyValue::new("attribute1", ATTRIBUTE_VALUES[index_first_attribute]),
+                    KeyValue::new("attribute2", ATTRIBUTE_VALUES[index_second_attribute]),
+                    KeyValue::new("attribute3", ATTRIBUTE_VALUES[index_third_attribute]),
+                    KeyValue::new("attribute4", ATTRIBUTE_VALUES[index_fourth_attribute]),
                 ],
             );
         });
@@ -86,13 +86,8 @@ fn counter_add_sorted(c: &mut Criterion) {
 }
 
 fn counter_add_unsorted(c: &mut Criterion) {
-    let attribute_values = [
-        "value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8", "value9",
-        "value10",
-    ];
-
     let counter = create_counter("Counter_Add_Unsorted");
-    c.bench_function("Counter_Add_Unsorted", |b| {        
+    c.bench_function("Counter_Add_Unsorted", |b| {
         b.iter(|| {
             // 4*4*10*10 = 1600 time series.
             let rands = CURRENT_RNG.with(|rng| {
@@ -111,10 +106,10 @@ fn counter_add_unsorted(c: &mut Criterion) {
             counter.add(
                 1,
                 &[
-                    KeyValue::new("attribute2", attribute_values[index_second_attribute]),
-                    KeyValue::new("attribute3", attribute_values[index_third_attribute]),
-                    KeyValue::new("attribute1", attribute_values[index_first_attribute]),
-                    KeyValue::new("attribute4", attribute_values[index_fourth_attribute]),
+                    KeyValue::new("attribute2", ATTRIBUTE_VALUES[index_second_attribute]),
+                    KeyValue::new("attribute3", ATTRIBUTE_VALUES[index_third_attribute]),
+                    KeyValue::new("attribute1", ATTRIBUTE_VALUES[index_first_attribute]),
+                    KeyValue::new("attribute4", ATTRIBUTE_VALUES[index_fourth_attribute]),
                 ],
             );
         });
@@ -122,18 +117,13 @@ fn counter_add_unsorted(c: &mut Criterion) {
 }
 
 fn counter_overflow(c: &mut Criterion) {
-    let attribute_values = [
-        "value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8", "value9",
-        "value10",
-    ];
-
     let counter = create_counter("Counter_Overflow");
     // Cause overflow.
     for v in 0..2001 {
         counter.add(100, &[KeyValue::new("A", v.to_string())]);
     }
 
-    c.bench_function("Counter_Overflow", |b| {        
+    c.bench_function("Counter_Overflow", |b| {
         b.iter(|| {
             // 4*4*10*10 = 1600 time series.
             let rands = CURRENT_RNG.with(|rng| {
@@ -152,12 +142,29 @@ fn counter_overflow(c: &mut Criterion) {
             counter.add(
                 1,
                 &[
-                    KeyValue::new("attribute2", attribute_values[index_second_attribute]),
-                    KeyValue::new("attribute3", attribute_values[index_third_attribute]),
-                    KeyValue::new("attribute1", attribute_values[index_first_attribute]),
-                    KeyValue::new("attribute4", attribute_values[index_fourth_attribute]),
+                    KeyValue::new("attribute2", ATTRIBUTE_VALUES[index_second_attribute]),
+                    KeyValue::new("attribute3", ATTRIBUTE_VALUES[index_third_attribute]),
+                    KeyValue::new("attribute1", ATTRIBUTE_VALUES[index_first_attribute]),
+                    KeyValue::new("attribute4", ATTRIBUTE_VALUES[index_fourth_attribute]),
                 ],
             );
+        });
+    });
+}
+
+fn random_generator(c: &mut Criterion) {
+    c.bench_function("ThreadLocal_Random_Generator_5", |b| {
+        b.iter(|| {
+            let __i1 = CURRENT_RNG.with(|rng| {
+                let mut rng = rng.borrow_mut();
+                [
+                    rng.gen_range(0..4),
+                    rng.gen_range(0..4),
+                    rng.gen_range(0..10),
+                    rng.gen_range(0..10),
+                    rng.gen_range(0..10),
+                ]
+            });
         });
     });
 }
