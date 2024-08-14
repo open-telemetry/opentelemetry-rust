443228183d509bf883e70b9c1be4545bb19d5659
Cijo Thomas <cijo.thomas@gmail.com>
2024-08-14 05:00:38 +0000

    add heklper

--- opentelemetry-sdk/benches/log_processor.rs
+++ opentelemetry-sdk/benches/log_processor.rs
@@ -1,13 +1,45 @@
-use std::{sync::{Arc, Mutex}, thread::sleep, time::SystemTime};
+/*
+    The benchmark results:
+    criterion = "0.5.1"
+    OS: Ubuntu 22.04.3 LTS (5.15.146.1-microsoft-standard-WSL2)
+    Hardware: AMD EPYC 7763 64-Core Processor - 2.44 GHz, 16vCPUs,
+    RAM: 64.0 GB
+    | Test                                        | Average time|
+    |---------------------------------------------|-------------|
+    | log_noop_processor                          | 134 ns      |
+    | log_cloning_processor                       | 236 ns      |
+    | log_clone_and_send_to_channel_processor     | 403 ns      |
+*/
+
+use std::{
+    sync::{Arc, Mutex},
+    thread::sleep,
+    time::SystemTime,
+};
 
 use criterion::{criterion_group, criterion_main, Criterion};
-use opentelemetry::logs::{
-    LogRecord as _, LogResult, Logger as _, LoggerProvider as _, Severity,
+use opentelemetry::logs::{LogRecord as _, LogResult, Logger as _, LoggerProvider as _, Severity};
+use opentelemetry_sdk::{
+    export::logs::LogData,
+    logs::{LogProcessor, LogRecord, Logger, LoggerProvider},
 };
-use opentelemetry_sdk::{export::logs::LogData, logs::{LogProcessor, LoggerProvider}};
 
 // Run this benchmark with:
 // cargo bench --bench log_processor
+
+fn create_log_record(logger: &Logger) -> LogRecord {
+    let mut log_record = logger.create_log_record();
+    let now = SystemTime::now();
+    log_record.set_observed_timestamp(now);
+    log_record.set_target("my-target".to_string());
+    log_record.set_event_name("CheckoutFailed");
+    log_record.set_severity_number(Severity::Warn);
+    log_record.set_severity_text("WARN");
+    log_record.add_attribute("book_id", "12345");
+    log_record.add_attribute("book_title", "Rust Programming Adventures");
+    log_record.add_attribute("message", "Unable to process checkout.");
+    log_record
+}
 
 #[derive(Debug)]
 struct NoopProcessor;
@@ -39,9 +71,7 @@ impl LogProcessor for CloningProcessor {
     fn shutdown(&self) -> LogResult<()> {
         Ok(())
     }
-
 }
-
 
 #[derive(Debug)]
 struct SendToChannelProcessor {
@@ -52,19 +82,20 @@ struct SendToChannelProcessor {
 impl SendToChannelProcessor {
     fn new() -> Self {
         let (sender, receiver) = std::sync::mpsc::channel();
-        let s = Self {             
-             sender : sender,
-             receiver : Arc::new(Mutex::new(receiver))
-            };
+        let s = Self {
+            sender: sender,
+            receiver: Arc::new(Mutex::new(receiver)),
+        };
         let receiver_cloned = s.receiver.clone();
-        let _ = std::thread::spawn(move || {
-            loop {
-                sleep(std::time::Duration::from_millis(10));
-                let data = receiver_cloned.lock().unwrap().recv();
-                if data.is_err() {
-                    println!("Error receiving log data from channel {0}", data.err().unwrap());
-                    break;
-                }
+        let _ = std::thread::spawn(move || loop {
+            sleep(std::time::Duration::from_millis(10));
+            let data = receiver_cloned.lock().unwrap().recv();
+            if data.is_err() {
+                println!(
+                    "Error receiving log data from channel {0}",
+                    data.err().unwrap()
+                );
+                break;
             }
         });
         s
@@ -74,7 +105,7 @@ impl SendToChannelProcessor {
 impl LogProcessor for SendToChannelProcessor {
     fn emit(&self, data: &mut LogData) {
         let data_cloned = data.clone();
-        let res =  self.sender.send(data_cloned);
+        let res = self.sender.send(data_cloned);
         if res.is_err() {
             println!("Error sending log data to channel {0}", res.err().unwrap());
         }
@@ -103,17 +134,7 @@ fn log_noop_processor(c: &mut Criterion) {
 
     c.bench_function("log_noop_processor", |b| {
         b.iter(|| {
-            let mut log_record = logger.create_log_record();
-            let now = SystemTime::now();
-            log_record.set_observed_timestamp(now);
-            log_record.set_target("my-target".to_string());
-            log_record.set_event_name("CheckoutFailed");
-            log_record.set_severity_number(Severity::Warn);
-            log_record.set_severity_text("WARN");
-            log_record.add_attribute("book_id", "12345");
-            log_record.add_attribute("book_title", "Rust Programming Adventures");
-            log_record.add_attribute("message", "Unable to process checkout.");
-
+            let log_record = create_log_record(&logger);
             logger.emit(log_record);
         });
     });
@@ -127,17 +148,7 @@ fn log_cloning_processor(c: &mut Criterion) {
 
     c.bench_function("log_cloning_processor", |b| {
         b.iter(|| {
-            let mut log_record = logger.create_log_record();
-            let now = SystemTime::now();
-            log_record.set_observed_timestamp(now);
-            log_record.set_target("my-target".to_string());
-            log_record.set_event_name("CheckoutFailed");
-            log_record.set_severity_number(Severity::Warn);
-            log_record.set_severity_text("WARN");
-            log_record.add_attribute("book_id", "12345");
-            log_record.add_attribute("book_title", "Rust Programming Adventures");
-            log_record.add_attribute("message", "Unable to process checkout.");
-
+            let log_record = create_log_record(&logger);
             logger.emit(log_record);
         });
     });
@@ -151,17 +162,7 @@ fn log_cloning_and_send_to_channel_processor(c: &mut Criterion) {
 
     c.bench_function("log_clone_and_send_to_channel_processor", |b| {
         b.iter(|| {
-            let mut log_record = logger.create_log_record();
-            let now = SystemTime::now();
-            log_record.set_observed_timestamp(now);
-            log_record.set_target("my-target".to_string());
-            log_record.set_event_name("CheckoutFailed");
-            log_record.set_severity_number(Severity::Warn);
-            log_record.set_severity_text("WARN");
-            log_record.add_attribute("book_id", "12345");
-            log_record.add_attribute("book_title", "Rust Programming Adventures");
-            log_record.add_attribute("message", "Unable to process checkout.");
-
+            let log_record = create_log_record(&logger);
             logger.emit(log_record);
         });
     });
