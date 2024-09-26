161929d8b693e02ca3fa6efdd5008e7730bf0f59
Laurent Quérel <laurent.querel@gmail.com>
2024-09-26 06:11:37 -0700

    Use weaver for semantic convention codegen (#2098)
    Co-authored-by: Zhongyang Wu <zhongyang.wu@outlook.com>
    Co-authored-by: Lalit Kumar Bhasin <labhas@microsoft.com>

--- opentelemetry-semantic-conventions/scripts/templates/header_attribute.rs
+++
@@ -1,3 +0,0 @@
-//! # Semantic Attributes
-//!
-//! The entire set of semantic attributes (or [conventions](https://opentelemetry.io/docs/concepts/semantic-conventions/)) defined by the project. The resource, metric, and trace modules reference these attributes.
