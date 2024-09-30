b5c53afb3606fea81db74038aa00ff9297166c88
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-09-30 11:08:01 -0700

    pin tonic to prev version

--- scripts/patch_dependencies.sh
+++ scripts/patch_dependencies.sh
@@ -8,6 +8,7 @@ function patch_version() {
 
 patch_version cc 1.0.105
 patch_version url 2.5.0
+patch_version tonic 0.12.2
 patch_version hyper-rustls 0.27.2 # 0.27.3 needs rustc v1.70.0
 patch_version tokio-util 0.7.11 # 0.7.12 needs rustc v1.70.0
 patch_version tokio-stream 0.1.15 # 0.1.16 needs rustc v1.70.0
