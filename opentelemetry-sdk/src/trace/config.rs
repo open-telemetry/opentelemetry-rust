d82829aa0c0149b9c6e2e7a29802c9289f332cad
Lalit <labhas@microsoft.com>
2024-10-27 21:12:34 -0700

    review comment,  remove unused import

--- opentelemetry-sdk/src/trace/config.rs
+++ opentelemetry-sdk/src/trace/config.rs
@@ -4,7 +4,7 @@
 //! can be set for the default OpenTelemetry limits and Sampler.
 use crate::trace::{span_limit::SpanLimits, IdGenerator, RandomIdGenerator, Sampler, ShouldSample};
 use crate::Resource;
-use opentelemetry::{otel_debug, otel_warn};
+use opentelemetry::otel_warn;
 use std::borrow::Cow;
 use std::env;
 use std::str::FromStr;
