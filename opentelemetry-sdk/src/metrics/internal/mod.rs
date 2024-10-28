7de4eb60d910e37f479b76a055f05c103ce6e4df
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-10-28 11:47:08 -0700

    remove global handler

--- opentelemetry-sdk/src/metrics/internal/mod.rs
+++ opentelemetry-sdk/src/metrics/internal/mod.rs
@@ -146,7 +146,6 @@ impl<AU: AtomicallyUpdate<T>, T: Number, O: Operation> ValueMap<AU, T, O> {
             let new_tracker = AU::new_atomic_tracker(self.buckets_count);
             O::update_tracker(&new_tracker, measurement, index);
             trackers.insert(STREAM_OVERFLOW_ATTRIBUTES.clone(), Arc::new(new_tracker));
-            global::handle_error(MetricError::Other("Warning: Maximum data points for metric stream exceeded. Entry added to overflow. Subsequent overflows to same metric until next collect will not be logged.".into()));
             otel_warn!( name: "ValueMap.measure",
                 message = "Maximum data points for metric stream exceeded. Entry added to overflow. Subsequent overflows to same metric until next collect will not be logged."
             );
