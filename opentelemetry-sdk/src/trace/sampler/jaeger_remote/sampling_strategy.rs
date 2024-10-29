c0999d05bf818f716eb1213ee50fcf09d0634063
Lalit <labhas@microsoft.com>
2024-10-29 00:17:38 -0700

    changed mutex poisioning error message

--- opentelemetry-sdk/src/trace/sampler/jaeger_remote/sampling_strategy.rs
+++ opentelemetry-sdk/src/trace/sampler/jaeger_remote/sampling_strategy.rs
@@ -109,7 +109,7 @@ impl Inner {
             .unwrap_or_else(|_err| {
                 otel_warn!(
                     name: "JaegerRemoteSampler.MutexPoisoned",
-                    message = "Unable to update Jaeger Remote sampling strategy: the sampler's internal mutex is poisoned, likely due to a panic in another thread holding the lock. The last known configuration will continue to be used until the remote sampling client is restarted.",
+                    message = "Unable to update Jaeger Remote sampling strategy: the sampler's internal mutex is poisoned, likely due to a panic in another thread holding the lock. The last known configuration will continue to be used until the application is restarted.",
                 );
             });
     }
