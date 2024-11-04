6c2bfcb329ed08846115709135367af109c0d78b
Lalit <labhas@microsoft.com>
2024-11-03 21:19:03 -0800

    initial commit

--- scripts/integration_tests.sh
+++ scripts/integration_tests.sh
@@ -1,1 +1,11 @@
-cd ./opentelemetry-otlp/tests/integration_test/tests  && cargo test -- --ignored
+# Run tests with the grpc-tonic feature
+cd ./opentelemetry-otlp/tests/integration_test/tests && cargo test --no-default-features --features "tonic-client" -- --ignored
+
+# Run tests with the reqwest-client feature
+#cd ./opentelemetry-otlp/tests/integration_test/tests && cargo test --no-default-features --features "reqwest-client" -- --ignored
+
+# Run tests with the reqwest-blocking-client feature
+#cd ./opentelemetry-otlp/tests/integration_test/tests && cargo test --no-default-features --features "reqwest-blocking-client" -- --ignored
+
+# Run tests with the hyper-client feature
+#cd ./opentelemetry-otlp/tests/integration_test/tests && cargo test --no-default-features --features "hyper-client" -- --ignored
