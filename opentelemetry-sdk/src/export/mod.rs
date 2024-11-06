e7e879d0a7cf146847072be540d45c94543adf0b
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-11-05 16:57:26 -0800

    Update mod.rs

--- opentelemetry-sdk/src/export/mod.rs
+++ opentelemetry-sdk/src/export/mod.rs
@@ -8,7 +8,7 @@ pub mod logs;
 #[cfg_attr(docsrs, doc(cfg(feature = "trace")))]
 pub mod trace;
 
-/// Marker trait for errors returned by exporters
+/// Trait for errors returned by exporters
 pub trait ExportError: std::error::Error + Send + Sync + 'static {
     /// The name of exporter that returned this error
     fn exporter_name(&self) -> &'static str;
