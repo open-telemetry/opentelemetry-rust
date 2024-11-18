f442e41a2287b2baf1d6befa1518a27dec903435
Utkarsh Umesan Pillai <66651184+utpilla@users.noreply.github.com>
2024-11-18 07:35:13 +0000

    Fix CI

--- stress/src/throughput.rs
+++ stress/src/throughput.rs
@@ -69,66 +69,66 @@ where
 
     thread::scope(|s| {
         s.spawn(|| {
-                let mut last_collect_time = Instant::now();
-                let mut total_count_old: u64 = 0;
-        
-                #[cfg(feature = "stats")]
-                let pid = Pid::from(std::process::id() as usize);
-                #[cfg(feature = "stats")]
-                let mut system = System::new_all();
-        
-                loop {
-                    let current_time = Instant::now();
-                    let elapsed = current_time.duration_since(last_collect_time).as_secs();
-                    if elapsed >= SLIDING_WINDOW_SIZE {
-                        let total_count_u64 = shared_mutable_stats_slice.sum();
-                        last_collect_time = Instant::now();
-                        let current_count = total_count_u64 - total_count_old;
-                        total_count_old = total_count_u64;
-                        let throughput = current_count / elapsed;
-                        println!(
-                            "Throughput: {} iterations/sec",
-                            throughput.to_formatted_string(&Locale::en)
-                        );
-        
-                        #[cfg(feature = "stats")]
-                        {
-                            system.refresh_all();
-                            if let Some(process) = system.process(pid) {
-                                println!(
-                                    "Memory usage: {:.2} MB",
-                                    process.memory() as f64 / (1024.0 * 1024.0)
-                                );
-                                println!("CPU usage: {}%", process.cpu_usage() / num_threads as f32);
-                                println!(
-                                    "Virtual memory usage: {:.2} MB",
-                                    process.virtual_memory() as f64 / (1024.0 * 1024.0)
-                                );
-                            } else {
-                                println!("Process not found");
-                            }
+            let mut last_collect_time = Instant::now();
+            let mut total_count_old: u64 = 0;
+
+            #[cfg(feature = "stats")]
+            let pid = Pid::from(std::process::id() as usize);
+            #[cfg(feature = "stats")]
+            let mut system = System::new_all();
+
+            loop {
+                let current_time = Instant::now();
+                let elapsed = current_time.duration_since(last_collect_time).as_secs();
+                if elapsed >= SLIDING_WINDOW_SIZE {
+                    let total_count_u64 = shared_mutable_stats_slice.sum();
+                    last_collect_time = Instant::now();
+                    let current_count = total_count_u64 - total_count_old;
+                    total_count_old = total_count_u64;
+                    let throughput = current_count / elapsed;
+                    println!(
+                        "Throughput: {} iterations/sec",
+                        throughput.to_formatted_string(&Locale::en)
+                    );
+
+                    #[cfg(feature = "stats")]
+                    {
+                        system.refresh_all();
+                        if let Some(process) = system.process(pid) {
+                            println!(
+                                "Memory usage: {:.2} MB",
+                                process.memory() as f64 / (1024.0 * 1024.0)
+                            );
+                            println!("CPU usage: {}%", process.cpu_usage() / num_threads as f32);
+                            println!(
+                                "Virtual memory usage: {:.2} MB",
+                                process.virtual_memory() as f64 / (1024.0 * 1024.0)
+                            );
+                        } else {
+                            println!("Process not found");
                         }
-        
-                        println!("\n");
                     }
-        
-                    if STOP.load(Ordering::SeqCst) {
-                        break;
-                    }
-        
-                    thread::sleep(Duration::from_millis(5000));
+
+                    println!("\n");
                 }
+
+                if STOP.load(Ordering::SeqCst) {
+                    break;
+                }
+
+                thread::sleep(Duration::from_millis(5000));
+            }
         });
 
         for thread_index in 0..num_threads {
             let func_arc_clone = Arc::clone(&func_arc);
-            s.spawn(move || {
-                loop {
-                    func_arc_clone();
-                    unsafe { shared_mutable_stats_slice.increment(thread_index); }
-                    if STOP.load(Ordering::SeqCst) {
-                        break;
-                    }
+            s.spawn(move || loop {
+                func_arc_clone();
+                unsafe {
+                    shared_mutable_stats_slice.increment(thread_index);
+                }
+                if STOP.load(Ordering::SeqCst) {
+                    break;
                 }
             });
         }
@@ -160,6 +160,9 @@ impl<'a> UnsafeSlice<'a> {
 
     #[inline(always)]
     fn sum(&self) -> u64 {
-        self.slice.iter().map(|cell| unsafe { (*cell.get()).count }).sum()
+        self.slice
+            .iter()
+            .map(|cell| unsafe { (*cell.get()).count })
+            .sum()
     }
 }
