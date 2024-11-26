62145a03215dd6b4b797791235796d495efdf0d7
Cijo Thomas <cijo.thomas@gmail.com>
2024-11-26 07:35:51 -0800

    few tweaks based on feedback

--- opentelemetry/src/lib.rs
+++ opentelemetry/src/lib.rs
@@ -1,7 +1,5 @@
 //! Implements the [`API`] component of [OpenTelemetry].
 //!
-//! *[Supported Rust Versions](#supported-rust-versions)*
-//!
 //! [`API`]: https://opentelemetry.io/docs/specs/otel/overview/#api
 //! [OpenTelemetry]: https://opentelemetry.io/docs/what-is-opentelemetry/
 //!
@@ -240,22 +238,6 @@
 //! [`Prometheus`]: https://prometheus.io
 //! [`Zipkin`]: https://zipkin.io
 //!
-//! ## Supported Rust Versions
-//!
-//! OpenTelemetry is built against the latest stable release. The minimum
-//! supported version is 1.70. The current OpenTelemetry version is not
-//! guaranteed to build on Rust versions earlier than the minimum supported
-//! version.
-//! This crate is built against the latest stable release. The minimum supported
-//! version is 1.70. The current version is not guaranteed to build on Rust
-//! versions earlier than the minimum supported version.
-//!
-//! The current stable Rust compiler and the three most recent minor versions
-//! before it will always be supported. For example, if the current stable
-//! compiler version is 1.49, the minimum supported version will not be
-//! increased past 1.46, three minor versions prior. Increasing the minimum
-//! supported compiler version is not considered a semver breaking change as
-//! long as doing so complies with this policy.
 #![warn(
     future_incompatible,
     missing_debug_implementations,
