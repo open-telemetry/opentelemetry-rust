2f462797ecf17ce32ac86d14bbf751577819417d
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-11-26 11:13:54 -0800

    Update opentelemetry-sdk/src/logs/log_emitter.rs
    Co-authored-by: Cijo Thomas <cithomas@microsoft.com>

--- opentelemetry-sdk/src/logs/log_emitter.rs
+++ opentelemetry-sdk/src/logs/log_emitter.rs
@@ -240,7 +240,7 @@ impl Logger {
 
     #[deprecated(
         since = "0.27.1",
-        note = "This method is intended for appender developers, but has no defined use-case in typical workflows. It is deprecated and will be removed in the next major release."
+        note = "This method was intended for appender developers, but has no defined use-case in typical workflows. It is deprecated and will be removed in the next major release."
     )]
     /// LoggerProvider associated with this logger.
     pub fn provider(&self) -> &LoggerProvider {
