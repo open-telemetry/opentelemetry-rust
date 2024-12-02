6b71301b9945879909b7eb66fa5e5fe18818043d
Cijo Thomas <cijo.thomas@gmail.com>
2024-12-02 10:01:08 -0800

    Use static dispatch in SimpleLogProcessor (#2338)
    Co-authored-by: Lalit Kumar Bhasin <lalit_fin@yahoo.com>

--- opentelemetry-sdk/src/logs/mod.rs
+++ opentelemetry-sdk/src/logs/mod.rs
@@ -49,7 +49,7 @@ mod tests {
         let exporter: InMemoryLogExporter = InMemoryLogExporter::default();
         let logger_provider = LoggerProvider::builder()
             .with_resource(resource.clone())
-            .with_log_processor(SimpleLogProcessor::new(Box::new(exporter.clone())))
+            .with_log_processor(SimpleLogProcessor::new(exporter.clone()))
             .build();
 
         // Act
