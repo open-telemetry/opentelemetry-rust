d96d83d9284328202e957183a7dbfac51a8647d9
David Pitoniak <davidpitoniak32@gmail.com>
2024-12-04 19:48:09 -0500

    feat: add with_schema_url for ResourceBuilder

--- opentelemetry-sdk/src/resource/builder.rs
+++ opentelemetry-sdk/src/resource/builder.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{borrow::Cow, time::Duration};
 
 use opentelemetry::{KeyValue, Value};
 
@@ -61,6 +61,25 @@ impl ResourceBuilder {
         self.with_attribute(KeyValue::new(SERVICE_NAME, name.into()))
     }
 
+    /// This will merge the provided `schema_url` with the current state of the Resource being built. It
+    /// will use the following rules to determine which `schema_url` should be used.
+    ///
+    /// ### [Schema url]
+    /// Schema url is determined by the following rules, in order:
+    /// 1. If the current builder resource doesn't have a `schema_url`, the provided `schema_url` will be used.
+    /// 2. If the current builder resource has a `schema_url`, and the provided `schema_url` is different from the builder resource, `schema_url` will be empty.
+    /// 3. If the provided `schema_url` is the same as the current builder resource, it will be used.
+    ///
+    /// [Schema url]: https://github.com/open-telemetry/opentelemetry-specification/blob/v1.9.0/specification/schemas/overview.md#schema-url
+    pub fn with_schema_url<KV, S>(mut self, attributes: KV, schema_url: S) -> Self
+    where
+        KV: IntoIterator<Item = KeyValue>,
+        S: Into<Cow<'static, str>>,
+    {
+        self.resource = Resource::from_schema_url(attributes, schema_url).merge(&self.resource);
+        self
+    }
+
     /// Create a [Resource] with the options provided to the [ResourceBuilder].
     pub fn build(self) -> Resource {
         self.resource
@@ -74,6 +93,49 @@ mod tests {
     use crate::resource::EnvResourceDetector;
 
     use super::*;
+
+    #[test]
+    fn with_schema_url() {
+        let test_cases = vec![
+            (Some("http://schema/a"), Some("http://schema/b"), None),
+            (None, Some("http://schema/b"), Some("http://schema/b")),
+            (
+                Some("http://schema/a"),
+                Some("http://schema/a"),
+                Some("http://schema/a"),
+            ),
+        ];
+
+        for (schema_url_a, schema_url_b, expected_schema_url) in test_cases.into_iter() {
+            let base_builder = if let Some(url) = schema_url_a {
+                ResourceBuilder {
+                    resource: Resource::from_schema_url(vec![KeyValue::new("key", "")], url),
+                }
+            } else {
+                ResourceBuilder::new()
+            };
+
+            let resource = base_builder
+                .with_schema_url(
+                    vec![KeyValue::new("key", "")],
+                    schema_url_b.expect("should always be Some for this test"),
+                )
+                .build();
+
+            assert_eq!(
+                resource.schema_url().map(|s| s as &str),
+                expected_schema_url,
+                "Merging schema_url_a {:?} with schema_url_b {:?} did not yield expected result {:?}",
+                schema_url_a, schema_url_b, expected_schema_url
+            );
+        }
+
+        // if only one resource contains key value pairs
+        let resource = Resource::from_schema_url(vec![], "http://schema/a");
+        let other_resource = Resource::new(vec![KeyValue::new("key", "")]);
+
+        assert_eq!(resource.merge(&other_resource).schema_url(), None);
+    }
 
     #[test]
     fn detect_resource() {
