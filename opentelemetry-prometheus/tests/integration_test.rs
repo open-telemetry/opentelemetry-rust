a56ea8db0c1c5d711228bfb5d79321819784ebd0
Kudlaty <kfuks2@gmail.com>
2024-12-05 21:51:58 +0100

    fix: add explicit histogram boundaries to metrics
    This commit introduces predefined boundary constants for histograms and applies them in several test cases within the Prometheus exporter integration test. It eliminates the use of the experimental metrics view feature by removing its configuration from the SdkMeterProvider setup and the dependencies.
    
    Change taken from @ttys3, thanks :)
    
    Co-authored-by: ttys3 <ttys3.rust@gmail.com>

--- opentelemetry-prometheus/tests/integration_test.rs
+++ opentelemetry-prometheus/tests/integration_test.rs
@@ -7,13 +7,22 @@ use opentelemetry::metrics::{Meter, MeterProvider as _};
 use opentelemetry::KeyValue;
 use opentelemetry::{InstrumentationScope, Key};
 use opentelemetry_prometheus::{ExporterBuilder, ResourceSelector};
-use opentelemetry_sdk::metrics::{new_view, Aggregation, Instrument, SdkMeterProvider, Stream};
+use opentelemetry_sdk::metrics::SdkMeterProvider;
 use opentelemetry_sdk::resource::{
     EnvResourceDetector, SdkProvidedResourceDetector, TelemetryResourceDetector,
 };
 use opentelemetry_sdk::Resource;
 use opentelemetry_semantic_conventions::resource::{SERVICE_NAME, TELEMETRY_SDK_VERSION};
 use prometheus::{Encoder, TextEncoder};
+
+const BOUNDARIES: &[f64] = &[
+    0.0, 5.0, 10.0, 25.0, 50.0, 75.0, 100.0, 250.0, 500.0, 1000.0,
+];
+
+const BYTES_BOUNDARIES: &[f64] = &[
+    0.0, 5.0, 10.0, 25.0, 50.0, 75.0, 100.0, 250.0, 500.0, 750.0, 1000.0, 2500.0, 5000.0, 7500.0,
+    10000.0,
+];
 
 #[ignore = "https://github.com/open-telemetry/opentelemetry-rust/pull/2224"]
 #[test]
@@ -123,6 +132,7 @@ fn prometheus_exporter_integration() {
                     .f64_histogram("histogram_baz")
                     .with_description("a very nice histogram")
                     .with_unit("By")
+                    .with_boundaries(BOUNDARIES.to_vec())
                     .build();
                 histogram.record(23.0, &attrs);
                 histogram.record(7.0, &attrs);
@@ -185,6 +195,7 @@ fn prometheus_exporter_integration() {
                 let histogram = meter
                     .f64_histogram("invalid.hist.name")
                     .with_description("a histogram with an invalid name")
+                    .with_boundaries(BOUNDARIES.to_vec())
                     .build();
                 histogram.record(23.0, &attrs);
             }),
@@ -374,18 +385,6 @@ fn prometheus_exporter_integration() {
         let provider = SdkMeterProvider::builder()
             .with_resource(res)
             .with_reader(exporter)
-            .with_view(
-                new_view(
-                    Instrument::new().name("histogram_*"),
-                    Stream::new().aggregation(Aggregation::ExplicitBucketHistogram {
-                        boundaries: vec![
-                            0.0, 5.0, 10.0, 25.0, 50.0, 75.0, 100.0, 250.0, 500.0, 1000.0,
-                        ],
-                        record_min_max: true,
-                    }),
-                )
-                .unwrap(),
-            )
             .build();
 
         let scope = InstrumentationScope::builder("testmeter")
@@ -559,6 +558,7 @@ fn duplicate_metrics() {
                     .u64_histogram("foo")
                     .with_unit("By")
                     .with_description("meter histogram foo")
+                    .with_boundaries(BYTES_BOUNDARIES.to_vec())
                     .build();
 
                 foo_a.record(100, &[KeyValue::new("A", "B")]);
@@ -567,6 +567,7 @@ fn duplicate_metrics() {
                     .u64_histogram("foo")
                     .with_unit("By")
                     .with_description("meter histogram foo")
+                    .with_boundaries(BYTES_BOUNDARIES.to_vec())
                     .build();
 
                 foo_b.record(100, &[KeyValue::new("A", "B")]);
@@ -631,6 +632,7 @@ fn duplicate_metrics() {
                     .u64_histogram("bar")
                     .with_unit("By")
                     .with_description("meter a bar")
+                    .with_boundaries(BYTES_BOUNDARIES.to_vec())
                     .build();
 
                 bar_a.record(100, &[KeyValue::new("A", "B")]);
@@ -639,6 +641,7 @@ fn duplicate_metrics() {
                     .u64_histogram("bar")
                     .with_unit("By")
                     .with_description("meter b bar")
+                    .with_boundaries(BYTES_BOUNDARIES.to_vec())
                     .build();
 
                 bar_b.record(100, &[KeyValue::new("A", "B")]);
@@ -702,6 +705,7 @@ fn duplicate_metrics() {
                     .u64_histogram("bar")
                     .with_unit("By")
                     .with_description("meter histogram bar")
+                    .with_boundaries(BYTES_BOUNDARIES.to_vec())
                     .build();
 
                 bar_a.record(100, &[KeyValue::new("A", "B")]);
@@ -710,6 +714,7 @@ fn duplicate_metrics() {
                     .u64_histogram("bar")
                     .with_unit("ms")
                     .with_description("meter histogram bar")
+                    .with_boundaries(BYTES_BOUNDARIES.to_vec())
                     .build();
 
                 bar_b.record(100, &[KeyValue::new("A", "B")]);
@@ -759,6 +764,7 @@ fn duplicate_metrics() {
                     .u64_histogram("foo")
                     .with_unit("By")
                     .with_description("meter histogram foo")
+                    .with_boundaries(BOUNDARIES.to_vec())
                     .build();
 
                 foo_histogram_a.record(100, &[KeyValue::new("A", "B")]);
