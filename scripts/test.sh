80741cd589af6b76470e5e07cab3bb1386c32dec
Dylan Thacker-Smith <dylan.ah.smith@gmail.com>
2025-01-22 18:18:47 +0000

    Remove opentelemetry-prometheus as a workspace member

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,6 +13,10 @@ cargo test --workspace --all-features --lib
 echo "Running tests for opentelemetry package with --no-default-features"
 cargo test --manifest-path=opentelemetry/Cargo.toml --no-default-features --lib
 
+# Run tests for non-workspace member crate
+echo "Running tests for opentelemetry-prometheus with --all-features"
+(cd opentelemetry-prometheus && cargo test --all-features --lib)
+
 # Run global tracer provider test in single thread
 # //TODO: This tests were not running for a while. Need to find out how to run
 # run them. Using --ignored will run other tests as well, so that cannot be used.
