1c47111bfd3cf05131a482b69f394b3b56fdf1e9
Cijo Thomas <cijo.thomas@gmail.com>
2025-01-23 16:06:32 -0800

    Move inmemory exporter for spans to diff namespace

--- opentelemetry-sdk/src/testing/mod.rs
+++ opentelemetry-sdk/src/testing/mod.rs
@@ -1,5 +1,6 @@
 //! In-Memory exporters for testing purpose.
 
+/// Structs used for testing
 #[cfg(all(feature = "testing", feature = "trace"))]
 pub mod trace;
 
--- opentelemetry-sdk/src/trace/mod.rs
+++ opentelemetry-sdk/src/trace/mod.rs
@@ -25,6 +25,14 @@ pub use config::{config, Config};
 pub use events::SpanEvents;
 pub use export::{ExportResult, SpanData, SpanExporter};
 
+/// In-Memory span exporter for testing purpose.
+#[cfg(any(feature = "testing", test))]
+#[cfg_attr(docsrs, doc(cfg(any(feature = "testing", test))))]
+pub mod in_memory_exporter;
+#[cfg(any(feature = "testing", test))]
+#[cfg_attr(docsrs, doc(cfg(any(feature = "testing", test))))]
+pub use in_memory_exporter::{InMemorySpanExporter, InMemorySpanExporterBuilder};
+
 pub use id_generator::{IdGenerator, RandomIdGenerator};
 pub use links::SpanLinks;
 pub use provider::{Builder, TracerProvider};
@@ -50,8 +58,8 @@ mod tests {
 
     use super::*;
     use crate::{
-        testing::trace::{InMemorySpanExporter, InMemorySpanExporterBuilder},
         trace::span_limit::{DEFAULT_MAX_EVENT_PER_SPAN, DEFAULT_MAX_LINKS_PER_SPAN},
+        trace::{InMemorySpanExporter, InMemorySpanExporterBuilder},
     };
     use opentelemetry::trace::{
         SamplingDecision, SamplingResult, SpanKind, Status, TraceContextExt, TraceState,
