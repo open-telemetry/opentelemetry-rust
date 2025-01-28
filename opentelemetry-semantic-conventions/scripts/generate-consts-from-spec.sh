86f291b717ca5c976b671be4d84e19fa09d1bdea
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2025-01-28 09:46:58 -0800

    bump

--- opentelemetry-semantic-conventions/scripts/generate-consts-from-spec.sh
+++ opentelemetry-semantic-conventions/scripts/generate-consts-from-spec.sh
@@ -5,8 +5,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CRATE_DIR="${SCRIPT_DIR}/../"
 
 # freeze the spec version and generator version to make generation reproducible
-SPEC_VERSION=1.29.0
-WEAVER_VERSION=v0.11.0
+SPEC_VERSION=1.30.0
+WEAVER_VERSION=v0.12.0
 
 cd "$CRATE_DIR"
 
