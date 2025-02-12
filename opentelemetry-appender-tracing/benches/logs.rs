05920759303358cb979b7b5538da2b6b5c0be8ee
Drew Relmas <drewrelmas@microsoft.com>
2025-02-12 14:11:23 -0800

    Update appender-tracing pprof usage with Windows exclusion (#2655)

--- opentelemetry-appender-tracing/benches/logs.rs
+++ opentelemetry-appender-tracing/benches/logs.rs
@@ -20,6 +20,7 @@ use opentelemetry_sdk::error::OTelSdkResult;
 use opentelemetry_sdk::logs::{LogBatch, LogExporter};
 use opentelemetry_sdk::logs::{LogProcessor, SdkLogRecord, SdkLoggerProvider};
 use opentelemetry_sdk::Resource;
+#[cfg(not(target_os = "windows"))]
 use pprof::criterion::{Output, PProfProfiler};
 use tracing::error;
 use tracing_subscriber::prelude::*;
