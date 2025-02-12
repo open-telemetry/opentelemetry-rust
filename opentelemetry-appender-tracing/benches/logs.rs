79c98985a2d302284de4722e039bc725926ecbbe
Drew Relmas <drewrelmas@microsoft.com>
2025-02-12 13:33:15 -0800

    cargo fmt fix

--- opentelemetry-appender-tracing/benches/logs.rs
+++ opentelemetry-appender-tracing/benches/logs.rs
@@ -21,7 +21,7 @@ use opentelemetry_sdk::logs::{LogBatch, LogExporter};
 use opentelemetry_sdk::logs::{LogProcessor, SdkLogRecord, SdkLoggerProvider};
 use opentelemetry_sdk::Resource;
 #[cfg(not(target_os = "windows"))]
-use pprof::criterion::{Output, PProfProfiler}; 
+use pprof::criterion::{Output, PProfProfiler};
 use tracing::error;
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::Layer;
