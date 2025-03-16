761f338fbb88d890e748ba8a629cc8d5470f18b6
gruebel <anton.gruebel@gmail.com>
2025-03-16 10:20:51 +0100

    fix OTLP doctest

--- opentelemetry-otlp/src/lib.rs
+++ opentelemetry-otlp/src/lib.rs
@@ -153,15 +153,12 @@
 //!
 //!     let tracer_provider = opentelemetry_sdk::trace::SdkTracerProvider::builder()
 //!         .with_batch_exporter(exporter)
-//!         .with_config(
-//!             trace::Config::default()
-//!                 .with_sampler(Sampler::AlwaysOn)
-//!                 .with_id_generator(RandomIdGenerator::default())
-//!                 .with_max_events_per_span(64)
-//!                 .with_max_attributes_per_span(16)
-//!                 .with_max_events_per_span(16)
-//!                 .with_resource(Resource::builder_empty().with_attributes([KeyValue::new("service.name", "example")]).build()),
-//!         ).build();
+//!         .with_sampler(Sampler::AlwaysOn)
+//!         .with_id_generator(RandomIdGenerator::default())
+//!         .with_max_events_per_span(64)
+//!         .with_max_attributes_per_span(16)
+//!         .with_resource(Resource::builder_empty().with_attributes([KeyValue::new("service.name", "example")]).build())
+//!         .build();
 //!     global::set_tracer_provider(tracer_provider.clone());
 //!     let tracer = global::tracer("tracer-name");
 //!         # tracer
