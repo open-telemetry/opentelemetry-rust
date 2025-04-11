8e1ce8d4533d4d76e11226da3fb7bb2220cb9db6
Cijo Thomas <cijo.thomas@gmail.com>
2025-04-11 14:30:08 -0700

    fix asyncpr

--- opentelemetry-sdk/src/metrics/periodic_reader_with_async_runtime.rs
+++ opentelemetry-sdk/src/metrics/periodic_reader_with_async_runtime.rs
@@ -20,7 +20,7 @@ use crate::{
     Resource,
 };
 
-use super::{data::ResourceMetrics, pipeline::Pipeline, reader::MetricReader, InstrumentKind};
+use super::{data::ResourceMetrics, pipeline::Pipeline, reader::MetricReader, instrument::InstrumentKind};
 
 const DEFAULT_TIMEOUT: Duration = Duration::from_secs(30);
 const DEFAULT_INTERVAL: Duration = Duration::from_secs(60);
