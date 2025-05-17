e284385198c6f2f7b89e9328f62ffaa98f4ba528
gruebel <anton.gruebel@gmail.com>
2025-05-17 09:41:54 +0200

    fix windows check

--- scripts/msrv.sh
+++ scripts/msrv.sh
@@ -5,7 +5,9 @@ set -eu
 members=$(cargo metadata -q --no-deps --format-version 1 | jq -r '.packages[].manifest_path')
 
 for member in $members; do
-  echo "Verifying MSRV version for $member"
-  cargo msrv verify --manifest-path "$member" --output-format json
+  # needed for windows CI run
+  clean_member=$(printf '%s' "$member" | tr -d '\r')
+  echo "Verifying MSRV version for $clean_member"
+  cargo msrv verify --manifest-path "$clean_member" --output-format json
   echo "" # just for nicer separation between packages
 done
