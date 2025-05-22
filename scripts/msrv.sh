8c29ca7e21cf0c041695a50d683504f9cbeca515
Anton Grübel <anton.gruebel@gmail.com>
2025-05-22 17:39:53 +0200

    chore: leverage fallback resolver for MSRV check (#2993)

--- scripts/msrv.sh
+++ scripts/msrv.sh
@@ -2,55 +2,12 @@
 
 set -eu
 
-# Check if a version is specified as parameter
-if [ $# -eq 0 ]; then
-  echo "No Rust version specified. Usage: $0 <rust-version>"
-  exit 1
-fi
+members=$(cargo metadata -q --no-deps --format-version 1 | jq -r '.packages[].manifest_path')
 
-RUST_VERSION=$1
-
-# Determine the directory containing the script
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-
-# Path to the configuration file
-CONFIG_FILE="$SCRIPT_DIR/msrv_config.json"
-
-# Change to the root directory of the repository
-cd "$SCRIPT_DIR/.."
-
-echo "Current working directory: $(pwd)"
-
-# function to check if specified toolchain is installed
-check_rust_toolchain_installed() {
-  local version=$1
-  if ! rustup toolchain list | grep -q "$version"; then
-    echo "Rust toolchain $version is not installed. Please install it using 'rustup toolchain install $version'."
-    exit 1
-  fi
-}
-
-# check if specified toolchain is installed
-check_rust_toolchain_installed "$RUST_VERSION"
-
-# Extract the exact installed rust version string
-installed_version=$(rustup toolchain list | grep "$RUST_VERSION" | awk '{print $1}')
-
-# Read the configuration file and get the packages for the specified version
-if [ -f "$CONFIG_FILE" ]; then
-  packages=$(jq -r --arg version "$RUST_VERSION" '.[$version] | .[]' "$CONFIG_FILE" | tr '\n' ' ')
-  if [ -z "$packages" ]; then
-    echo "No packages found for Rust version $RUST_VERSION in the configuration file."
-    exit 1
-  fi
-else
-  echo "Configuration file $CONFIG_FILE not found."
-  exit 1
-fi
-
-# Check MSRV for the packages
-for package in $packages; do
-  package=$(echo "$package" | tr -d '\r\n') # Remove any newline and carriage return characters
-  echo "Command: rustup run \"$installed_version\" cargo check --manifest-path=\"$package\" --all-features"
-  rustup run "$installed_version" cargo check --manifest-path=$package --all-features
+for member in $members; do
+  # needed for windows CI run
+  clean_member=$(printf '%s' "$member" | tr -d '\r')
+  echo "Verifying MSRV version for $clean_member"
+  cargo msrv verify --manifest-path "$clean_member" --output-format json
+  echo "" # just for nicer separation between packages
 done
