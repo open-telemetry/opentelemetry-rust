0c8d7e58ad9f3ce9260c1399170ce604d3409998
Andrea Corradi <andreac@unstable.it>
2025-07-31 16:42:25 +0200

    chore: update tonic and prost to 0.14

--- examples/tracing-grpc/build.rs
+++ examples/tracing-grpc/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/helloworld.proto")?;
+    tonic_prost_build::compile_protos("proto/helloworld.proto")?;
     Ok(())
 }
