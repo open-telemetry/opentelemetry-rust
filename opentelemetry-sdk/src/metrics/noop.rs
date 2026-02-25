ffd884eb74d8f84abcaec9ec3a1a749fcb78d896
Bryant Biggs <bryantbiggs@gmail.com>
2026-02-25 14:46:35 -0600

    feat: wire bind() through SDK ResolvedMeasures and NoopSyncInstrument

--- opentelemetry-sdk/src/metrics/noop.rs
+++ opentelemetry-sdk/src/metrics/noop.rs
@@ -1,5 +1,5 @@
 use opentelemetry::{
-    metrics::{InstrumentProvider, SyncInstrument},
+    metrics::{BoundSyncInstrument, InstrumentProvider, SyncInstrument},
     KeyValue,
 };
 
@@ -33,6 +33,20 @@ impl NoopSyncInstrument {
 
 impl<T> SyncInstrument<T> for NoopSyncInstrument {
     fn measure(&self, _value: T, _attributes: &[KeyValue]) {
+        // Ignored
+    }
+
+    fn bind(&self, _attributes: &[KeyValue]) -> Box<dyn BoundSyncInstrument<T>> {
+        Box::new(NoopBoundSyncInstrument { _private: () })
+    }
+}
+
+struct NoopBoundSyncInstrument {
+    _private: (),
+}
+
+impl<T> BoundSyncInstrument<T> for NoopBoundSyncInstrument {
+    fn measure(&self, _measurement: T) {
         // Ignored
     }
 }
