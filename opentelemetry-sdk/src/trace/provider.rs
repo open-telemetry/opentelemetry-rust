83717350d019f4ae5a9282e2a46d424667a499ce
Charly Molter <charly.molter@konghq.com>
2026-03-06 16:36:00 +0100

    fix: service.name fallback
    The fallback introduced in #3302 doesn't work properly as the fallback didn't
    read from `process.executable.name` as indicated in the [spec](https://opentelemetry.io/docs/specs/semconv/resource/service/#service).
    
    We now read from the resource when it exists, we also do the fallback when building the Resource so that any possible override is done before falling back
    
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- opentelemetry-sdk/src/trace/provider.rs
+++ opentelemetry-sdk/src/trace/provider.rs
@@ -629,9 +629,9 @@ mod tests {
                 .get(&Key::from_static_str(SERVICE_NAME))
                 .map(|v| v.to_string())
                 .unwrap();
-            assert!(
-                service_name.starts_with("unknown_service:opentelemetry_sdk-"),
-                "Expected service name to start with 'unknown_service:opentelemetry_sdk-', got: {}",
+            assert_eq!(
+                service_name, "unknown_service",
+                "Expected service name to be 'unknown_service', got: {}",
                 service_name
             );
             assert_telemetry_resource(&default_config_provider);
@@ -660,9 +660,9 @@ mod tests {
                     .get(&Key::from_static_str(SERVICE_NAME))
                     .map(|v| v.to_string())
                     .unwrap();
-                assert!(
-                    service_name.starts_with("unknown_service:opentelemetry_sdk-"),
-                    "Expected service name to start with 'unknown_service:opentelemetry_sdk-', got: {}",
+                assert_eq!(
+                    service_name, "unknown_service",
+                    "Expected service name to be 'unknown_service', got: {}",
                     service_name
                 );
                 assert_resource(&env_resource_provider, "key1", Some("value1"));
@@ -693,9 +693,9 @@ mod tests {
                     .get(&Key::from_static_str(SERVICE_NAME))
                     .map(|v| v.to_string())
                     .unwrap();
-                assert!(
-                    service_name.starts_with("unknown_service:opentelemetry_sdk-"),
-                    "Expected service name to start with 'unknown_service:opentelemetry_sdk-', got: {}",
+                assert_eq!(
+                    service_name, "unknown_service",
+                    "Expected service name to be 'unknown_service', got: {}",
                     service_name
                 );
                 assert_resource(
