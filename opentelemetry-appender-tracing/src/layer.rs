921d6cf6450f90d84604892281e1fc2067ec4076
cijothomas <cijo.thomas@gmail.com>
2026-05-05 19:08:18 -0700

    Fix cargo fmt

--- opentelemetry-appender-tracing/src/layer.rs
+++ opentelemetry-appender-tracing/src/layer.rs
@@ -337,9 +337,10 @@ impl std::fmt::Debug for TracingSpanAttributes {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self.0 {
             TracingSpanAttributesInner::All => f.write_str("TracingSpanAttributes::all()"),
-            TracingSpanAttributesInner::Allowlist(set) => {
-                f.debug_tuple("TracingSpanAttributes::allowlist").field(set).finish()
-            }
+            TracingSpanAttributesInner::Allowlist(set) => f
+                .debug_tuple("TracingSpanAttributes::allowlist")
+                .field(set)
+                .finish(),
         }
     }
 }
