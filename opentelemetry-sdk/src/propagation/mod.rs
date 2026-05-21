aa3cf45fbcd5079b416ce9968f5ca452e8adfefb
jsurany <jonathan.surany@gmail.com>
2026-05-21 15:26:19 -0400

    add prototype for EnvVarsCarrier

--- opentelemetry-sdk/src/propagation/mod.rs
+++ opentelemetry-sdk/src/propagation/mod.rs
@@ -1,6 +1,8 @@
 //! OpenTelemetry Propagators
 mod baggage;
+mod env_vars_carrier;
 mod trace_context;
 
 pub use baggage::BaggagePropagator;
+pub use env_vars_carrier::EnvVarsCarrier;
 pub use trace_context::TraceContextPropagator;
